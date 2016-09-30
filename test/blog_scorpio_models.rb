b9024d774f5b54801ca786d0450be4423e226852
Ethan <ethan@unth>
2016-09-30 15:42:16 -0700

    update index test for schemas in api doc, schema_keys def, base_url

--- test/blog_scorpio_models.rb
+++ test/blog_scorpio_models.rb
@@ -1,11 +1,13 @@
 class Article < Scorpio::Model
   self.resource_name = 'articles'
+  self.schema_keys = ['articles']
+  self.base_url = 'https://blog.example.com/'
 
   set_api_description(YAML.load_file('test/blog_description.yml'))
 
   class << self
     def connection
-      Faraday.new(:url => 'https://blog.example.com') do |c|
+      Faraday.new do |c|
         c.request :json
         c.request :api_hammer_request_logger, Blog.logger
         c.adapter :rack, Blog.new
