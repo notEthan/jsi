bea1fd55e4d0af373b955e588c3b9350392e4c88
Ethan <ethan@unth>
2016-10-04 23:32:34 -0700

    add response to HTTPError

--- lib/scorpio.rb
+++ lib/scorpio.rb
@@ -5,6 +5,7 @@ module Scorpio
   class Error < StandardError; end
   class HTTPError < Error
     define_singleton_method(:status) { |status| Scorpio.error_classes_by_status[status] = self }
+    attr_accessor :response
   end
   class ClientError < HTTPError; end
   class ServerError < HTTPError; end
