bf55261b52bea3ae8e3fd7428fe306b1e51eda2a
Ethan <ethan@unth>
2017-03-28 16:11:26 -0700

    request schema failure raises its own error, RequestSchemaFailure

--- lib/scorpio/model.rb
+++ lib/scorpio/model.rb
@@ -5,6 +5,8 @@ require 'faraday_middleware'
 module Scorpio
   # see also Faraday::Env::MethodsWithBodies
   METHODS_WITH_BODIES = %w(post put patch options)
+  class RequestSchemaFailure < Error
+  end
 
   class Model
     class << self
@@ -315,7 +317,7 @@ module Scorpio
       end
 
       def request_schema_fail(object, schema)
-        raise("object does not conform to schema.\nobject = #{object.inspect}\nschema = #{JSON.pretty_generate(schema, quirks_mode: true)}")
+        raise(RequestSchemaFailure, "object does not conform to schema.\nobject = #{object.inspect}\nschema = #{JSON.pretty_generate(schema, quirks_mode: true)}")
       end
 
       def response_object_to_instances(object, schema, initialize_options = {})
