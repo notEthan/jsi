2428ee6456696518d40354c9ea620c77f4603bdc
Ethan <ethan@unth>
2017-10-10 19:21:22 -0700

    typelike modules look for #object_group_text

--- lib/scorpio/typelike_modules.rb
+++ lib/scorpio/typelike_modules.rb
@@ -1,12 +1,14 @@
 module Scorpio
   module Hashlike
     def inspect
-      "\#{<#{self.class.name}> #{self.map { |k, v| "#{k.inspect} => #{v.inspect}" }.join(', ')}}"
+      object_group_text = respond_to?(:object_group_text) ? ' ' + self.object_group_text : ''
+      "\#{<#{self.class.name}#{object_group_text}> #{self.map { |k, v| "#{k.inspect} => #{v.inspect}" }.join(', ')}}"
     end
 
     def pretty_print(q)
       q.instance_exec(self) do |obj|
-        text "\#{<#{obj.class.name}>"
+        object_group_text = obj.respond_to?(:object_group_text) ? ' ' + obj.object_group_text : ''
+        text "\#{<#{obj.class.name}#{object_group_text}>"
         group_sub {
           nest(2) {
             breakable(obj.any? { true } ? ' ' : '')
@@ -26,12 +28,14 @@ module Scorpio
   end
   module Arraylike
     def inspect
-      "\#[<#{self.class.name}> #{self.map { |e| e.inspect }.join(', ')}]"
+      object_group_text = respond_to?(:object_group_text) ? ' ' + self.object_group_text : ''
+      "\#[<#{self.class.name}#{object_group_text}> #{self.map { |e| e.inspect }.join(', ')}]"
     end
 
     def pretty_print(q)
       q.instance_exec(self) do |obj|
-        text "\#[<#{obj.class.name}>"
+        object_group_text = obj.respond_to?(:object_group_text) ? ' ' + obj.object_group_text : ''
+        text "\#[<#{obj.class.name}#{object_group_text}>"
         group_sub {
           nest(2) {
             breakable(obj.any? { true } ? ' ' : '')
