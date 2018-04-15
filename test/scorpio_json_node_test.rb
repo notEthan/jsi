74c61b45ac025695db071f9fe27c383421bb3852
Ethan <ethan@unth>
2018-04-15 01:25:01 -0700

    Scorpio::JSON::Node test more

--- test/scorpio_json_node_test.rb
+++ test/scorpio_json_node_test.rb
@@ -41,6 +41,31 @@ describe Scorpio::JSON::Node do
       assert_equal('b', Scorpio::JSON::Node.new([0, {'x' => [{'a' => ['b']}]}], [1, 'x', 0, 'a', 0]).content)
     end
   end
+  describe '#deref' do
+    let(:document) do
+      {
+        'foo' => {'bar' => ['baz']},
+        'a' => {'$ref' => '#/foo'}, # not sure a description is actually allowed here, whatever
+      }
+    end
+    it 'follows a $ref' do
+      assert_equal({'bar' => ['baz']}, node['a'].deref.content)
+    end
+    it 'returns the node when there is no $ref to follow' do
+      assert_equal({'bar' => ['baz']}, node['foo'].deref.content)
+    end
+    describe "dealing with google's invalid $refs" do
+      let(:document) do
+        {
+          'schemas' => {'bar' => ['baz']},
+          'a' => {'$ref' => 'bar'},
+        }
+      end
+      it 'looks for a node in #/schemas with the name of the $ref' do
+        assert_equal(['baz'], node['a'].deref.content)
+      end
+    end
+  end
   describe '#[]' do
     describe 'without dereferencing' do
       let(:document) { [0, {'x' => [{'a' => ['b']}]}] }
@@ -77,10 +102,69 @@ describe Scorpio::JSON::Node do
         assert_equal(['baz'], subscripted.content)
         assert_equal(['foo', 'bar'], subscripted.path)
       end
-      it 'does not follow a $ref when subscripting ' do
+      it 'does not follow a $ref when subscripting a key that is present' do
         subscripted = node['a']['description']
         assert_equal('hi', subscripted)
       end
+    end
+  end
+  describe '#document_node' do
+    let(:document) { {'a' => {'b' => 3}} }
+    it 'has content that is the document' do
+      assert_equal({'a' => {'b' => 3}}, node['a'].document_node.content)
+    end
+  end
+  describe '#pointer_path' do
+    let(:document) { {'a' => {'b' => 3}} }
+    it 'is empty' do
+      assert_equal('', node.pointer_path)
+    end
+    it 'is not empty' do
+      assert_equal('/a', node['a'].pointer_path)
+    end
+    describe 'containing an empty string and some slashes and tildes that need escaping' do
+      let(:document) { {'' => {'a/b~c!d#e[f]' => []}} }
+      it 'matches' do
+        assert_equal('//a~1b~0c!d#e[f]', node['']['a/b~c!d#e[f]'].pointer_path)
+      end
+    end
+  end
+  describe '#fragment' do
+    let(:document) { {'a' => {'b' => 3}} }
+    it 'is empty' do
+      assert_equal('#', node.fragment)
+    end
+    it 'is not empty' do
+      assert_equal('#/a', node['a'].fragment)
+    end
+    describe 'containing an empty string and some slashes and tildes that need escaping' do
+      let(:document) { {'' => {'a/b~c!d#e[f]' => []}} }
+      it 'matches' do
+        assert_equal('#//a~1b~0c!d#e%5Bf%5D', node['']['a/b~c!d#e[f]'].fragment)
+      end
+    end
+  end
+  describe '#fingerprint' do
+    it 'hashes consistently' do
+      assert_equal('x', {Scorpio::JSON::Node.new([0], []) => 'x'}[Scorpio::JSON::Node.new([0], [])])
+    end
+    it 'hashes consistently regardless of the Node being decorated as a subclass' do
+      assert_equal('x', {Scorpio::JSON::Node.new_by_type([0], []) => 'x'}[Scorpio::JSON::Node.new([0], [])])
+      assert_equal('x', {Scorpio::JSON::Node.new([0], []) => 'x'}[Scorpio::JSON::Node.new_by_type([0], [])])
+    end
+    it '==' do
+      assert_equal(Scorpio::JSON::Node.new([0], []), Scorpio::JSON::Node.new([0], []))
+      assert_equal(Scorpio::JSON::Node.new_by_type([0], []), Scorpio::JSON::Node.new([0], []))
+      assert_equal(Scorpio::JSON::Node.new([0], []), Scorpio::JSON::Node.new_by_type([0], []))
+      assert_equal(Scorpio::JSON::Node.new_by_type([0], []), Scorpio::JSON::Node.new_by_type([0], []))
+    end
+    it '!=' do
+      refute_equal(Scorpio::JSON::Node.new([0], []), Scorpio::JSON::Node.new({}, []))
+      refute_equal(Scorpio::JSON::Node.new_by_type([0], []), Scorpio::JSON::Node.new({}, []))
+      refute_equal(Scorpio::JSON::Node.new([0], []), Scorpio::JSON::Node.new_by_type({}, []))
+      refute_equal(Scorpio::JSON::Node.new_by_type([0], []), Scorpio::JSON::Node.new_by_type({}, []))
+      refute_equal({}, Scorpio::JSON::Node.new_by_type({}, []))
+      refute_equal(Scorpio::JSON::Node.new_by_type({}, []), {})
     end
   end
 end
