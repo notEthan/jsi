ea5fa2d6b6e948d11fa44a5e22a057f41271ecba
Ethan <ethan@unth>
2018-04-16 00:15:10 -0700

    Hashlike categorization of Hash methods by safe vs destructive, key-only vs key/value, and what returns a modified copy

--- lib/scorpio/typelike_modules.rb
+++ lib/scorpio/typelike_modules.rb
@@ -1,5 +1,16 @@
 module Scorpio
   module Hashlike
+    # safe methods which can be delegated to #to_hash (which the includer is assumed to have defined).
+    # 'safe' means, in this context, nondestructive - methods which do not modify the receiver.
+
+    # methods which do not need to access the value.
+    SAFE_KEY_ONLY_METHODS = %w(each_key empty? has_key? include? key? keys length member? size)
+    SAFE_KEY_VALUE_METHODS = %w(< <= > >= any? assoc compact dig each_pair each_value fetch fetch_values has_value? invert key merge rassoc reject select to_h to_proc transform_values value? values values_at)
+    DESTRUCTIVE_METHODS = %w(clear delete delete_if keep_if reject! replace select! shift)
+    # methods which return a modified copy, which you'd expect to be of the same class as the receiver.
+    # there are some ambiguous ones that are omitted, like #invert.
+    SAFE_MODIFIED_COPY_METHODS = %w(compact merge reject select transform_values)
+
     def inspect
       object_group_text = respond_to?(:object_group_text) ? ' ' + self.object_group_text : ''
       "\#{<#{self.class.name}#{object_group_text}> #{self.map { |k, v| "#{k.inspect} => #{v.inspect}" }.join(', ')}}"
