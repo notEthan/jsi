6c38ca1953510fa9d5fca6c25a1fcac69153bc7a
Ethan <ethan@unth>
2018-04-30 19:44:11 -0700

    test SchemaObjectBase []= with hash and array describing schemas, respectively with and without hash or array like objects

--- test/schema_object_base_test.rb
+++ test/schema_object_base_test.rb
@@ -170,6 +170,82 @@ describe Scorpio::SchemaObjectBase do
       end
     end
   end
+  describe 'hashlike []=' do
+    let(:schema_content) do
+      {
+        'type' => 'object',
+        'properties' => {
+          'foo' => {'type' => 'object'},
+        },
+      }
+    end
+    let(:document) do
+      {'foo' => {'x' => 'y'}}
+    end
+    it 'sets a property' do
+      orig_foo = subject['foo']
+
+      subject['foo'] = {'y' => 'z'}
+
+      assert_equal({'y' => 'z'}, subject['foo'].as_json)
+      assert_instance_of(Scorpio.class_for_schema(schema.schema_node['properties']['foo']), orig_foo)
+      assert_instance_of(Scorpio.class_for_schema(schema.schema_node['properties']['foo']), subject['foo'])
+    end
+    it 'updates to a modified copy of the object without altering the original' do
+      orig_object = subject.object
+
+      subject['foo'] = {'y' => 'z'}
+
+      refute_equal(orig_object, subject.object)
+      assert_equal({'x' => 'y'}, orig_object['foo'].as_json)
+      assert_equal({'y' => 'z'}, subject.object['foo'].as_json)
+      assert_equal(orig_object.class, subject.object.class)
+    end
+    describe 'when the object is not hashlike' do
+      let(:object) { nil }
+      it 'errors' do
+        err = assert_raises(NoMethodError) { subject['foo'] = 0 }
+        assert_match(%r(\Aundefined method `\[\]=' for #<Scorpio::SchemaClasses::X.*>\z), err.message)
+      end
+    end
+  end
+  describe 'arraylike []=' do
+    let(:schema_content) do
+      {
+        'type' => 'array',
+        'items' => {},
+      }
+    end
+    let(:document) do
+      ['foo', true, ['q']]
+    end
+    it 'sets an index' do
+      orig_2 = subject[2]
+
+      subject[2] = {'y' => 'z'}
+
+      assert_equal({'y' => 'z'}, subject[2].as_json)
+      assert_instance_of(Scorpio.class_for_schema(schema.schema_node['items']), orig_2)
+      assert_instance_of(Scorpio.class_for_schema(schema.schema_node['items']), subject[2])
+    end
+    it 'updates to a modified copy of the object without altering the original' do
+      orig_object = subject.object
+
+      subject[2] = {'y' => 'z'}
+
+      refute_equal(orig_object, subject.object)
+      assert_equal(['q'], orig_object[2].as_json)
+      assert_equal({'y' => 'z'}, subject.object[2].as_json)
+      assert_equal(orig_object.class, subject.object.class)
+    end
+    describe 'when the object is not arraylike' do
+      let(:object) { nil }
+      it 'errors' do
+        err = assert_raises(NoMethodError) { subject[2] = 0 }
+        assert_match(%r(\Aundefined method `\[\]=' for #<Scorpio::SchemaClasses::X.*>\z), err.message)
+      end
+    end
+  end
   describe 'property accessors' do
     let(:schema_content) do
       {
