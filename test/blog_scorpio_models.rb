4b7c85fc85bc26bb32b793ed7beaf762ec316c11
Ethan <ethan@unth>
2018-05-24 21:33:30 -0700

    BlogModel.logger

--- test/blog_scorpio_models.rb
+++ test/blog_scorpio_models.rb
@@ -1,15 +1,22 @@
+require 'logger'
+
 # this is a virtual model to parent models representing resources of the blog. it sets
 # up connection information including base url, custom middleware or adapter for faraday.
 # it describes the API by setting the API document, but this class itself represents no
 # resources - it sets no resource_name and defines no schema_keys.
 class BlogModel < Scorpio::ResourceBase
+  define_inheritable_accessor(:logger)
+  logpath = Pathname.new('log/test.log')
+  FileUtils.mkdir_p(logpath.dirname)
+  self.logger = ::Logger.new(logpath)
+
   if ENV['SCORPIO_API_SPECIFIER'] == 'rest_description'
     self.openapi_document = Scorpio::Google::RestDescription.new(YAML.load_file('test/blog.rest_description.yml')).to_openapi_document
   else
     self.openapi_document = YAML.load_file('test/blog.openapi.yml')
   end
   self.base_url = File.join('https://blog.example.com/', openapi_document.basePath)
-  self.faraday_request_middleware = [[:api_hammer_request_logger, Blog.logger]]
+  self.faraday_request_middleware = [[:api_hammer_request_logger, logger]]
   self.faraday_adapter = [:rack, Blog.new]
 end
 
