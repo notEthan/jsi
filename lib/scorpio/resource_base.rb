b1d9beabce512b6ac4ebddbf139bad9ee236c06f
Ethan <ethan@unth>
2018-06-14 18:26:15 -0700

    simplify request_body_for_schema. skip ResourceBase#represent_for_schema (didn't do anything anyway). this will be rewritten when I have a request class.

--- lib/scorpio/resource_base.rb
+++ lib/scorpio/resource_base.rb
@@ -284,7 +284,7 @@ module Scorpio
             body = request_body_for_schema(model_attributes, operation.request_schema)
           elsif call_params.is_a?(Hash)
             body = request_body_for_schema(model_attributes.merge(call_params), operation.request_schema)
-            body.update(call_params)
+            body = body.merge(call_params) # TODO
           else
             body = call_params
           end
@@ -380,7 +380,11 @@ module Scorpio
       def request_body_for_schema(object, schema)
         if object.is_a?(Scorpio::ResourceBase)
           # TODO request_schema_fail unless schema is for given model type 
-          request_body_for_schema(object.represent_for_schema(schema), schema)
+          request_body_for_schema(object.attributes, schema)
+        elsif object.is_a?(Scorpio::SchemaInstanceBase)
+          request_body_for_schema(object.instance, schema)
+        elsif object.is_a?(Scorpio::JSON::Node)
+          request_body_for_schema(object.content, schema)
         else
           if object.is_a?(Hash)
             object.map do |key, value|
@@ -525,11 +529,6 @@ module Scorpio
       end
 
       response
-    end
-
-    # TODO
-    def represent_for_schema(schema)
-      @attributes
     end
 
     def as_json
