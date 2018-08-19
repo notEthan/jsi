3bbe726efdcfcfef805b81fae0806340d9bcf654
Ethan <ethan@unth>
2018-08-19 15:07:58 -0700

    JSI::Bug

--- lib/jsi.rb
+++ lib/jsi.rb
@@ -3,4 +3,8 @@ require "pp"
 require "jsi/json-schema-fragments"
 
 module JSI
+  # generally put in code paths that are not expected to be valid control flow paths.
+  # rather a NotImplementedCorrectlyError. but that's too long.
+  class Bug < NotImplementedError
+  end
 end
