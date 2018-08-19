09bb8a91e5818c9ac5eb5182c185ac0208692288
Ethan <ethan@unth>
2018-08-19 15:08:28 -0700

    requires and autoloads

--- lib/jsi.rb
+++ lib/jsi.rb
@@ -1,10 +1,28 @@
 require "jsi/version"
 require "pp"
 require "jsi/json-schema-fragments"
+require "jsi/util"
 
 module JSI
   # generally put in code paths that are not expected to be valid control flow paths.
   # rather a NotImplementedCorrectlyError. but that's too long.
   class Bug < NotImplementedError
+  end
+
+  autoload :JSON, 'jsi/json'
+  autoload :Typelike, 'jsi/typelike_modules'
+  autoload :Hashlike, 'jsi/typelike_modules'
+  autoload :Arraylike, 'jsi/typelike_modules'
+  autoload :Schema, 'jsi/schema'
+  autoload :SchemaInstanceBase, 'jsi/schema_instance_base'
+  autoload :SchemaInstanceBaseArray, 'jsi/schema_instance_base'
+  autoload :SchemaInstanceBaseHash, 'jsi/schema_instance_base'
+  autoload :SchemaClasses, 'jsi/schema_instance_base'
+  autoload :ObjectJSONCoder, 'jsi/schema_instance_json_coder'
+  autoload :StructJSONCoder, 'jsi/struct_json_coder'
+  autoload :SchemaInstanceJSONCoder,'jsi/schema_instance_json_coder'
+
+  def self.class_for_schema(*a, &b)
+    SchemaClasses.class_for_schema(*a, &b)
   end
 end
--- lib/jsi/json.rb
+++ lib/jsi/json.rb
@@ -1,5 +1,7 @@
 module JSI
   module JSON
     autoload :Node, 'jsi/json/node'
+    autoload :ArrayNode, 'jsi/json/node'
+    autoload :HashNode, 'jsi/json/node'
   end
 end
