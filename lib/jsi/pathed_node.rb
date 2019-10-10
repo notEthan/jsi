985c7b41f3de080dacf42bc2429d1a8a1a143e27
Ethan <ethan@unth.net>
2019-10-09 22:05:27 -0700

    JSI::PathedHashNode, PathedArrayNode

--- lib/jsi/pathed_node.rb
+++ lib/jsi/pathed_node.rb
@@ -19,4 +19,91 @@ module JSI
       node_ptr.deref(node_document, &block)
     end
   end
+
+  # module extending a {JSI::PathedNode} object when its node_content is Hash-like (responds to #to_hash)
+  module PathedHashNode
+    # yields each hash key and value of this node.
+    #
+    # each yielded key is the same as a key of the node content hash,
+    # and each yielded value is the result of self[key] (see #[]).
+    #
+    # returns an Enumerator if no block is given.
+    #
+    # @yield [Object, Object] each key and value of this hash node
+    # @return [self, Enumerator]
+    def each
+      return to_enum(__method__) { node_content_hash_pubsend(:size) } unless block_given?
+      node_content_hash_pubsend(:each_key) { |k| yield(k, self[k]) }
+      self
+    end
+
+    # @return [Hash] a hash in which each key is a key of the node_content hash and
+    #   each value is the result of self[key] (see #[]).
+    def to_hash
+      {}.tap { |h| each_key { |k| h[k] = self[k] } }
+    end
+
+    include Hashlike
+
+    # @param method_name [String, Symbol]
+    # @param *a, &b are passed to the invocation of method_name
+    # @return [Object] the result of calling method method_name on the node_content or its #to_hash
+    def node_content_hash_pubsend(method_name, *a, &b)
+      if node_content.respond_to?(method_name)
+        node_content.public_send(method_name, *a, &b)
+      else
+        node_content.to_hash.public_send(method_name, *a, &b)
+      end
+    end
+
+    # methods that don't look at the value; can skip the overhead of #[] (invoked by #to_hash)
+    SAFE_KEY_ONLY_METHODS.each do |method_name|
+      define_method(method_name) do |*a, &b|
+        node_content_hash_pubsend(method_name, *a, &b)
+      end
+    end
+  end
+
+  module PathedArrayNode
+    # yields each array element of this node.
+    #
+    # each yielded element is the result of self[index] for each index of our array (see #[]).
+    #
+    # returns an Enumerator if no block is given.
+    #
+    # @yield [Object] each element of this array node
+    # @return [self, Enumerator]
+    def each
+      return to_enum(__method__) { node_content_ary_pubsend(:size) } unless block_given?
+      node_content_ary_pubsend(:each_index) { |i| yield(self[i]) }
+      self
+    end
+
+    # @return [Array] an array, the same size as the node_content, in which the
+    #   element at each index is the result of self[index] (see #[])
+    def to_ary
+      to_a
+    end
+
+    include Arraylike
+
+    # @param method_name [String, Symbol]
+    # @param *a, &b are passed to the invocation of method_name
+    # @return [Object] the result of calling method method_name on the node_content or its #to_ary
+    def node_content_ary_pubsend(method_name, *a, &b)
+      if node_content.respond_to?(method_name)
+        node_content.public_send(method_name, *a, &b)
+      else
+        node_content.to_ary.public_send(method_name, *a, &b)
+      end
+    end
+
+    # methods that don't look at the value; can skip the overhead of #[] (invoked by #to_a).
+    # we override these methods from Arraylike
+    SAFE_INDEX_ONLY_METHODS.each do |method_name|
+      define_method(method_name) do |*a, &b|
+        node_content_ary_pubsend(method_name, *a, &b)
+      end
+    end
+  end
 end
