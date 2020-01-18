a89cebc26f05816a3f624bcc0813bba97adc953d
Ethan <ethan@unth.net>
2020-01-18 07:26:42 -0800

    test JSI::MetaschemaNode initial

---
+++ test/metaschema_node_test.rb
@@ -0,0 +1,14 @@
+require_relative 'test_helper'
+
+describe JSI::MetaschemaNode do
+  let(:node_document) { {'properties' => {'properties' => {'additionalProperties' => {'$ref' => '#'}}}} }
+  let(:node_ptr) { JSI::JSON::Pointer[] }
+  let(:metaschema_root_ptr) { JSI::JSON::Pointer[] }
+  let(:root_schema_ptr) { JSI::JSON::Pointer[] }
+  let(:subject) { JSI::MetaschemaNode.new(node_document, node_ptr: node_ptr, metaschema_root_ptr: node_ptr, root_schema_ptr: node_ptr) }
+  describe 'initialization' do
+    it 'initializes' do
+      subject
+    end
+  end
+end
