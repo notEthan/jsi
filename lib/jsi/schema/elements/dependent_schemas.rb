8a246d0f7b50a71e1b4d915137ef09c77850c758
Ethan <ethan@unth.net>
2020-02-15 01:59:49 -0800

    Schema::Elements::DEPENDENT_SCHEMAS action :validate

--- lib/jsi/schema/elements/dependent_schemas.rb
+++ lib/jsi/schema/elements/dependent_schemas.rb
@@ -4,6 +4,30 @@ module JSI
   module Schema::Elements
     DEPENDENT_SCHEMAS = element_map do
       Schema::Element.new(keyword: 'dependentSchemas') do |element|
+        element.add_action(:validate) do
+          #> This keyword's value MUST be an object.
+          next if !keyword_value_hash?('dependentSchemas')
+          next if !instance.respond_to?(:to_hash)
+
+          #> This keyword specifies subschemas that are evaluated if the
+          #> instance is an object and contains a certain property.
+          #
+          #> If the object key is a property in the instance, the entire instance must validate
+          #> against the subschema. Its use is dependent on the presence of the property.
+          results = {}
+          schema_content['dependentSchemas'].each_key do |property_name|
+            if instance.key?(property_name)
+              results[property_name] = inplace_subschema_validate(['dependentSchemas', property_name])
+            end
+          end
+          inplace_results_validate(
+            results.each_value.all?(&:valid?),
+            'validation.keyword.dependentSchemas.invalid',
+            "instance object is not valid against all schemas corresponding to matched property names specified by `dependentSchemas`",
+            keyword: 'dependentSchemas',
+            results: results.each_value,
+          )
+        end
       end
     end
   end
