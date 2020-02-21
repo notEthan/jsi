29e079d496706d1025172436a5089f9a38a74d20
Ethan <ethan@unth.net>
2020-02-21 06:07:49 -0800

    JSI::Util::Virtual

--- lib/jsi/util.rb
+++ lib/jsi/util.rb
@@ -102,6 +102,25 @@ module JSI
         end
       end
     end
+
+    module Virtual
+      class InstantiationError < StandardError
+      end
+
+      # this virtual class is not intended to be instantiated except by its subclasses, which override #initialize
+      def initialize
+        # :nocov:
+        raise(InstantiationError, "cannot instantiate virtual class #{self.class}")
+        # :nocov:
+      end
+
+      # virtual_method is used to indicate that the method calling it must be implemented on the (non-virtual) subclass
+      def virtual_method
+        # :nocov:
+        raise(Bug, "class #{self.class} must implement #{caller_locations.first.label}")
+        # :nocov:
+      end
+    end
   end
   public
   extend Util
