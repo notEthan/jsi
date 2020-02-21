d37639b3c2e01dfd774136f1f3b627b0dbf3220f
Ethan <ethan@unth.net>
2020-02-21 06:17:21 -0800

    dev JSI::Validation::Result, FullResult, ValidityResult

--- lib/jsi/validation.rb
+++ lib/jsi/validation.rb
@@ -2,5 +2,12 @@
 
 module JSI
   module Validation
+    autoload :Result, 'jsi/validation/result'
+    autoload :FullResult, 'jsi/validation/result'
+    autoload :ValidityResult, 'jsi/validation/result'
+
+    VALID = ValidityResult.new(true).freeze
+
+    INVALID = ValidityResult.new(false).freeze
   end
 end
--- lib/jsi/validation/result.rb
+++ lib/jsi/validation/result.rb
@@ -2,5 +2,51 @@
 
 module JSI
   module Validation
+    # a result of validating an instance against schemas which describe it.
+    # virtual base class.
+    class Result
+      include Util::Virtual
+
+      # @return [Boolean] is the instance valid against its schemas?
+      def valid?
+        # :nocov:
+        virtual_method
+        # :nocov:
+      end
+    end
+
+    # a full result of validating an instance against its schemas, with each validation error
+    class FullResult < Result
+      def initialize
+        @validation_errors = Set.new
+        @schema_issues = Set.new
+      end
+
+      attr_reader :validation_errors
+      attr_reader :schema_issues
+
+      def valid?
+        validation_errors.empty?
+      end
+
+      def freeze
+        @validation_errors.each(&:freeze)
+        @schema_issues.each(&:freeze)
+        @validation_errors.freeze
+        @schema_issues.freeze
+        super
+      end
+    end
+
+    # a result indicating only whether an instance is valid against its schemas
+    class ValidityResult < Result
+      def initialize(valid)
+        @valid = valid
+      end
+
+      def valid?
+        @valid
+      end
+    end
   end
 end
