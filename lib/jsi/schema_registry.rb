4549bd1602c4182604131c7da79751024d17ff5e
Ethan <ethan@unth.net>
2020-02-29 00:06:30 -0800

    SQ SchemaRegistry

--- lib/jsi/schema_registry.rb
+++ lib/jsi/schema_registry.rb
@@ -4,6 +4,8 @@ module JSI
     end
     class Collision < Error
     end
+    class RelativeURIRegistration < Error
+    end
     class SchemaNotFound < Error
     end
 
@@ -17,55 +19,69 @@ module JSI
       @schemas.keys
     end
 
-    def register(schema)
+    def register(schema, base_uri: nil)
       unless schema.is_a?(JSI::Schema)
         raise(TypeError, "schema must be a JSI::Schema; got: #{schema.pretty_inspect}")
       end
-      content = schema.jsi_node_content
-      if content.respond_to?(:to_hash)
-        content = content.to_hash
-        if content.key?('$id') && content['$id'].respond_to?(:to_str)
-          keyword = '$id'
-          id = content['$id'].to_str
-        elsif content.key?('id') && content['id'].respond_to?(:to_str)
-          keyword = 'id'
-          id = content['id'].to_str
-        end
-      end
-      if id
-        auri = Addressable::URI.parse(id)
 
-        if auri.fragment == ''
-          auri.fragment = nil
-          id = auri.to_s
-        elsif auri.fragment
-          raise(Schema::IdHasFragment.new("schema #{keyword} has fragment. id: #{id}\nNOTE: a fragment is technically allowed in older JSON schema specifications. this is currently not supported, but support could be added. if you require this, please open an issue at https://github.com/notEthan/jsi/issues").tap { |e| e.id = id })
-        end
-        if @schemas.key?(auri)
-          if @schemas[auri] != schema
-            raise(Collision, "id collision on #{id}. existing: \n#{@schemas[auri].pretty_inspect.chomp}\nnew:\n#{schema.pretty_inspect.chomp}")
+      JSI::Util.ycomb do |rec|
+        proc do |node, base_uri|
+          if node.is_a?(JSI::Schema)
+            node.jsi_schema_nodes.each do |schema_node|
+              id = schema_node.id
+              if id
+                id_uri = base_uri ? base_uri.join(id) : Addressable::URI.parse(id)
+
+                if id_uri.relative?
+                  raise(RelativeURIRegistration, "cannot register relative id URI #{id}; would you kindly pass the param `base_uri`")
+                else
+                  if id_uri.fragment == ''
+                    id_uri.fragment = nil
+                    id = id_uri.to_s
+                  elsif id_uri.fragment
+                    raise(Schema::IdHasFragment.new("schema id must not have a fragment. id: #{id}\nNOTE: a fragment is technically allowed in older JSON schema specifications. this is currently not supported, but support could be added. if you require this, please open an issue at https://github.com/notEthan/jsi/issues").tap { |e| e.id = id })
+                  end
+                  if @schemas.key?(id_uri)
+                    if @schemas[id_uri] != schema
+                      raise(Collision, "id collision on #{id}. existing: \n#{@schemas[id_uri].pretty_inspect.chomp}\nnew:\n#{schema.pretty_inspect.chomp}")
+                    end
+                  else
+                    @schemas[id_uri] = schema
+                  end
+                end
+                base_uri = id_uri
+              end
+              if schema_node.respond_to?(:to_hash)
+                schema_node.to_hash.values.each { |v| rec.call(v, base_uri) }
+              elsif schema_node.respond_to?(:to_ary)
+                schema_node.to_ary.each { |e| rec.call(e, base_uri) }
+              end
+            end
+          elsif node.respond_to?(:to_hash)
+            node.to_hash.values.each { |v| rec.call(v, base_uri) }
+          elsif node.respond_to?(:to_ary)
+            node.to_ary.each { |e| rec.call(e, base_uri) }
           end
-        else
-          @schemas[auri] = schema
         end
-      end
+      end.call(schema, base_uri ? Addressable::URI.parse(base_uri) : nil)
+
+
     end
 
     def find(id)
-      auri = Addressable::URI.parse(id)
-
-      if auri.fragment
-        # TODO error handling fragment with invalid pointer
-        ptr = JSI::JSON::Pointer.from_fragment('#' + auri.fragment)
-        auri.fragment = nil
+      id_uri = Addressable::URI.parse(id)
+      if id_uri.fragment
+# TODO error handling fragment with invalid pointer
+        ptr = JSI::JSON::Pointer.from_fragment('#' + id_uri.fragment)
+        id_uri.fragment = nil
       else
         ptr = JSI::JSON::Pointer[]
       end
 
-      if @schemas.key?(auri)
-        ptr.evaluate(@schemas[auri])
+      if @schemas.key?(id_uri)
+        ptr.evaluate(@schemas[id_uri])
       else
-        raise(SchemaNotFound, "id #{id} not in JSI.schema.ids:\n#{JSI.schemas.ids.join("\n")}")
+        raise(SchemaNotFound, "id #{id} not in JSI.registered_schemas.ids:\n#{JSI.registered_schemas.ids.join("\n")}")
       end
     end
   end
