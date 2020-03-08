959bad4cc8cca86feff7014e475e35c4143d744b
Ethan <ethan@unth.net>
2020-03-08 10:16:12 -0700

    JSONSchemaOrgDraft04 JSONSchemaOrgDraft06 root_basic_schema

--- lib/schemas/json-schema.org/draft-04/schema.rb
+++ lib/schemas/json-schema.org/draft-04/schema.rb
@@ -2,5 +2,7 @@
 
 module JSI
   schema_content = ::JSON.parse(RESOURCES_PATH.join('json-schema.org/draft-04/schema').read)
-  JSONSchemaOrgDraft04 = MetaschemaNode.new(schema_content).jsi_schema_module
+  JSONSchemaOrgDraft04 = MetaschemaNode.new(schema_content,
+    root_basic_schema: JSI::BasicSchema::Draft04.new(JSI::JSON::Pointer[], schema_content)
+  ).jsi_schema_module
 end
--- lib/schemas/json-schema.org/draft-06/schema.rb
+++ lib/schemas/json-schema.org/draft-06/schema.rb
@@ -2,5 +2,7 @@
 
 module JSI
   schema_content = ::JSON.parse(RESOURCES_PATH.join('json-schema.org/draft-06/schema').read)
-  JSONSchemaOrgDraft06 = MetaschemaNode.new(schema_content).jsi_schema_module
+  JSONSchemaOrgDraft06 = MetaschemaNode.new(schema_content,
+    root_basic_schema: JSI::BasicSchema::Draft06.new(JSI::JSON::Pointer[], schema_content)
+  ).jsi_schema_module
 end
