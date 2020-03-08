ffc132488fec70d9d66c87c8094b2445fc6ae437
Ethan <ethan@unth.net>
2020-03-08 13:18:06 -0700

    register our metaschemas

--- lib/schemas/json-schema.org/draft-04/schema.rb
+++ lib/schemas/json-schema.org/draft-04/schema.rb
@@ -4,5 +4,5 @@ module JSI
   schema_content = ::JSON.parse(RESOURCES_PATH.join('json-schema.org/draft-04/schema').read)
   JSONSchemaOrgDraft04 = MetaschemaNode.new(schema_content,
     root_basic_schema: JSI::BasicSchema::Draft04.new(JSI::JSON::Pointer[], schema_content)
-  ).jsi_schema_module
+  ).tap(&:jsi_register_schema).jsi_schema_module
 end
--- lib/schemas/json-schema.org/draft-06/schema.rb
+++ lib/schemas/json-schema.org/draft-06/schema.rb
@@ -4,5 +4,5 @@ module JSI
   schema_content = ::JSON.parse(RESOURCES_PATH.join('json-schema.org/draft-06/schema').read)
   JSONSchemaOrgDraft06 = MetaschemaNode.new(schema_content,
     root_basic_schema: JSI::BasicSchema::Draft06.new(JSI::JSON::Pointer[], schema_content)
-  ).jsi_schema_module
+  ).tap(&:jsi_register_schema).jsi_schema_module
 end
