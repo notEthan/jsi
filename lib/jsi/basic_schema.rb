3f4f2619047abb3e46a25dc3b1c76fe0b2dfc88f
Ethan <ethan@unth.net>
2020-03-08 07:22:00 -0700

    X BasicSchema dev initial

--- lib/jsi/basic_schema.rb
+++ lib/jsi/basic_schema.rb
@@ -2,5 +2,33 @@
 
 module JSI
   class BasicSchema
+    include Util::Memoize
+    include Util::FingerprintHash
+
+    # @param ptr [JSI::JSON::Pointer] pointer to the schema in the document
+    # @param document [#to_hash, #to_ary, Boolean, Object] document containing the schema
+    def initialize(ptr, document)
+      unless ptr.is_a?(JSI::JSON::Pointer)
+        raise(TypeError, "ptr is not a JSI::JSON::Pointer: #{ptr.inspect}")
+      end
+      @ptr = ptr
+      @document = document
+
+      @schema_content = ptr.evaluate(document)
+    end
+
+    # document containing the schema content
+    attr_reader :document
+
+    # JSI::JSON::Pointer pointing to this schema within the document
+    attr_reader :ptr
+
+    # underlying schema content (boolean or ruby Hash / json object)
+    attr_reader :schema_content
+
+    # @private
+    def jsi_fingerprint
+      {class: self.class, ptr: @ptr, document: @document}
+    end
   end
 end
