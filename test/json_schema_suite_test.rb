a46d4496344a8c151573f261a404c8595a2b8b4e
Ethan <ethan@unth.net>
2020-03-08 10:13:17 -0700

    FIXUP(s) [or split the remotes shit?] json_schema_suite_test

--- test/json_schema_suite_test.rb
+++ test/json_schema_suite_test.rb
@@ -2,6 +2,26 @@ require_relative 'test_helper'
 
 JSONSchemaTestSchema = JSI::Schema.new(JSON.parse(JSI::TEST_RESOURCES_PATH.join('JSON-Schema-Test-Suite/test-schema.json').read))
 
+JSI::Util.ycomb do |rec|
+  proc do |subpath|
+    path = JSI::TEST_RESOURCES_PATH.join('JSON-Schema-Test-Suite/remotes').join(*subpath)
+
+    if path.directory?
+      path.children(with_directory = false).each { |c| rec.call(subpath + [c.to_s]) }
+    elsif path.file? && path.to_s =~ /\.json\z/
+      remote_content = ::JSON.parse(path.read)
+      id = File.join('http://localhost:1234/', *subpath)
+      if subpath == ['subSchemas.json']
+        subSchemas_schema = JSI::Schema.new({'additionalProperties' => {'$ref' => JSI::Schema.default_metaschema.id}})
+        subSchemas = subSchemas_schema.new_jsi(remote_content)
+        JSI.registered_schemas.register(subSchemas, schema_id: id)
+      else
+        JSI::Schema.new(remote_content, schema_id: id)
+      end
+    end
+  end
+end.call([])
+
 describe 'JSON Schema Test Suite' do
   describe 'validity' do
     drafts = [
@@ -18,7 +38,13 @@ describe 'JSON Schema Test Suite' do
             describe(subpath.join('/')) do
               JSONSchemaTestSchema.new_jsi(::JSON.parse(path.read)).map do |tests_desc|
                 describe(tests_desc.description) do
-                  let(:schema) { metaschema.new_jsi(JSI::Typelike.as_json(tests_desc['schema'])) }
+                  let(:schema) do
+                    begin
+                      metaschema.new_jsi(JSI::Typelike.as_json(tests_desc['schema'])).tap(&:pretty_inspect)
+                    rescue JSI::Schema::IdHasFragment
+                      skip('unsupported id with fragment')
+                    end
+                  end
                   tests_desc.tests.each do |test|
                     describe(test.description) do
                       let(:jsi) { schema.new_jsi(JSI::Typelike.as_json(test.data)) }
@@ -26,7 +52,7 @@ describe 'JSON Schema Test Suite' do
                         result = jsi.jsi_validate
                         if test.valid != result.valid?
                           if !test.valid && schema['format']
-                            # skip format errors
+                            skip('format validation')
                           else
                             assert(false, {
                               valid: test.valid,
