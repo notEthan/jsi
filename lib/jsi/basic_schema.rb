e1dc4ff475d0ef18d7419ae1c023104960cc1ac0
Ethan <ethan@unth.net>
2020-03-10 22:58:57 -0700

    X BasicSchema dev #[] #/ #inspect #pretty_print

--- lib/jsi/basic_schema.rb
+++ lib/jsi/basic_schema.rb
@@ -26,6 +26,51 @@ module JSI
     # underlying schema content (boolean or ruby Hash / json object)
     attr_reader :schema_content
 
+    # returns a subschema of this BasicSchema
+    #
+    # @param *tokens [Array[Object]] tokens appended to our ptr indicating the location of the subschema
+    # @return [JSI::BasicSchema] the subschema at the location indicated by *tokens
+    def [](*tokens)
+      self.class.new(ptr + JSI::JSON::Pointer[*tokens], document)
+    end
+
+    # returns a schema in the same document as this one at the given ptr
+    # @param ptr [JSI::JSON::Pointer] pointer to a schema in our document
+    # @return [JSI::BasicSchema] the schema in our document at the given pointer
+    def /(ptr)
+      if ptr.respond_to?(:to_ary)
+        ptr = JSI::JSON::Pointer[*ptr]
+      end
+      self.class.new(ptr, document)
+    end
+
+    # @return [String]
+    def inspect
+      "\#<#{object_group_text.join(' ')} #{schema_content.inspect}>"
+    end
+
+    def pretty_print(q)
+      q.text '#<'
+      q.text object_group_text.join(' ')
+      q.group_sub {
+        q.nest(2) {
+          q.breakable ' '
+          q.pp schema_content
+        }
+      }
+      q.breakable ''
+      q.text '>'
+    end
+
+    # @private
+    # @return [Array<String>]
+    def object_group_text
+      [
+        self.class.inspect,
+        ptr.uri,
+      ]
+    end
+
     # @private
     def jsi_fingerprint
       {class: self.class, ptr: @ptr, document: @document}
