455adcc50d1ff6d753bb1b0e34ecd6d4aad3769c
Ethan <ethan@unth.net>
2020-03-14 21:51:34 -0700

    SQ MSN root_basic_schema (metaschema_node_test)

--- test/metaschema_node_test.rb
+++ test/metaschema_node_test.rb
@@ -3,9 +3,15 @@ require_relative 'test_helper'
 describe JSI::MetaschemaNode do
   let(:jsi_document) { {'properties' => {'properties' => {'additionalProperties' => {'$ref' => '#'}}}} }
   let(:jsi_ptr) { JSI::JSON::Pointer[] }
+  let(:root_basic_schema) { JSI::BasicSchema.new(JSI::JSON::Pointer[], jsi_document) }
   let(:metaschema_root_ptr) { JSI::JSON::Pointer[] }
-  let(:root_schema_ptr) { JSI::JSON::Pointer[] }
-  let(:subject) { JSI::MetaschemaNode.new(jsi_document, jsi_ptr: jsi_ptr, metaschema_root_ptr: jsi_ptr, root_schema_ptr: jsi_ptr) }
+  let(:subject) do
+    JSI::MetaschemaNode.new(jsi_document,
+      jsi_ptr: jsi_ptr,
+      root_basic_schema: root_basic_schema,
+      metaschema_root_ptr: jsi_ptr,
+    )
+  end
   describe 'initialization' do
     it 'initializes' do
       subject
