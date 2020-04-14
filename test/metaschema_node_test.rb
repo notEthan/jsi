525c99f34d957ba2e5d4b2be66c5cd5ea7c360bc
Ethan <ethan@unth.net>
2020-04-14 03:23:40 -0700

    test improve JSI::MetaschemaNode test

--- test/metaschema_node_test.rb
+++ test/metaschema_node_test.rb
@@ -24,8 +24,11 @@ describe JSI::MetaschemaNode do
         root_schema_ptr: jsi_ptr,
       )
     end
-    it 'initializes' do
-      subject
+    it 'acts like a metaschema' do
+      assert_is_a(subject.jsi_schema_module, subject)
+      assert_is_a(subject.properties['properties'].jsi_schema_module, subject.properties)
+      assert_is_a(subject.jsi_schema_module, subject.properties['properties'])
+      assert_is_a(subject.jsi_schema_module, subject.properties['properties'].additionalProperties)
     end
   end
   describe 'json schema draft' do
