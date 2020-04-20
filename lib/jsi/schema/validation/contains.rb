837a38b178306d894ea6a917b1020d0b6d700046
Ethan <ethan@unth.net>
2020-04-20 13:32:08 -0700

    refactor Schema::Validation::Contains#internal_validate_contains

--- lib/jsi/schema/validation/contains.rb
+++ lib/jsi/schema/validation/contains.rb
@@ -8,14 +8,15 @@ module JSI
         # An array instance is valid against "contains" if at least one of its elements is valid against
         # the given schema.
         if result_builder.instance.respond_to?(:to_ary)
-          results = result_builder.instance.each_index.map do |i|
-            result_builder.child_subschema_validate(['contains'], [i])
+          results = {}
+          result_builder.instance.each_index do |i|
+            results[i] = result_builder.child_subschema_validate(['contains'], [i])
           end
           result_builder.validate(
-            results.any?(&:valid?),
+            results.values.any?(&:valid?),
             'instance array does not contain any items valid against `contains` schema value',
             keyword: 'contains',
-            results: results,
+            results: results.values,
           )
         end
       end
