629e2884f2f9df953f6747692e685f31871fbd3d
Ethan <ethan@unth.net>
2020-04-20 10:44:11 -0700

    Schema::Elements::UNEVALUATED_ITEMS action :validate

--- lib/jsi/schema/elements/unevaluated_items.rb
+++ lib/jsi/schema/elements/unevaluated_items.rb
@@ -2,8 +2,28 @@
 
 module JSI
   module Schema::Elements
+    # https://json-schema.org/draft/2020-12/draft-bhutton-json-schema-01#name-unevaluateditems
     UNEVALUATED_ITEMS = element_map do
       Schema::Element.new(keyword: 'unevaluatedItems') do |element|
+        element.add_action(:validate) do
+          next if !keyword?('unevaluatedItems')
+          next if !instance.respond_to?(:to_ary)
+          results = {}
+          instance.each_index do |i|
+            if !result.evaluated_tokens.include?(i)
+              results[i] = child_subschema_validate(i, ['unevaluatedItems'])
+            end
+          end
+
+          child_results_validate(
+            results.each_value.all?(&:valid?),
+            'validation.keyword.unevaluatedItems.invalid',
+            "instance array unevaluated items are not all valid against `unevaluatedItems` schema",
+            keyword: 'unevaluatedItems',
+            child_results: results,
+            instance_indexes_valid: results.inject({}) { |h, (i, r)| h.update({i => r.valid?}) }.freeze,
+          )
+        end
       end
     end
   end
