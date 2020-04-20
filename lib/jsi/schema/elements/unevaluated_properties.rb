cd2457c06c44d9d6a9369a1de92ca514dd3ff743
Ethan <ethan@unth.net>
2020-04-20 10:44:11 -0700

    Schema::Elements::UNEVALUATED_PROPERTIES action :validate

--- lib/jsi/schema/elements/unevaluated_properties.rb
+++ lib/jsi/schema/elements/unevaluated_properties.rb
@@ -2,8 +2,28 @@
 
 module JSI
   module Schema::Elements
+    # https://json-schema.org/draft/2020-12/draft-bhutton-json-schema-01#name-unevaluatedproperties
     UNEVALUATED_PROPERTIES = element_map do
       Schema::Element.new(keyword: 'unevaluatedProperties') do |element|
+        element.add_action(:validate) do
+          next if !keyword?('unevaluatedProperties')
+          next if !instance.respond_to?(:to_hash)
+          results = {}
+          instance.each_key do |property_name|
+            if !result.evaluated_tokens.include?(property_name)
+              results[property_name] = child_subschema_validate(property_name, ['unevaluatedProperties'])
+            end
+          end
+
+          child_results_validate(
+            results.each_value.all?(&:valid?),
+            'validation.keyword.unevaluatedProperties.invalid',
+            "instance object unevaluated properties are not all valid against `unevaluatedProperties` schema",
+            keyword: 'unevaluatedProperties',
+            child_results: results,
+            instance_properties_valid: results.inject({}) { |h, (k, r)| h.update({k => r.valid?}) }.freeze,
+          )
+        end
       end
     end
   end
