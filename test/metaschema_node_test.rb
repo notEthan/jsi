400ba0a048d254d4275eda4b89057f2cc3e132e8
Ethan <ethan@unth.net>
2020-04-24 01:43:58 -0700

    test refactor metaschema node test

--- test/metaschema_node_test.rb
+++ test/metaschema_node_test.rb
@@ -1,12 +1,29 @@
 require_relative 'test_helper'
 
 describe JSI::MetaschemaNode do
-  let(:jsi_document) { {'properties' => {'properties' => {'additionalProperties' => {'$ref' => '#'}}}} }
-  let(:jsi_ptr) { JSI::JSON::Pointer[] }
-  let(:metaschema_root_ptr) { JSI::JSON::Pointer[] }
-  let(:root_schema_ptr) { JSI::JSON::Pointer[] }
-  let(:subject) { JSI::MetaschemaNode.new(jsi_document, jsi_ptr: jsi_ptr, metaschema_root_ptr: jsi_ptr, root_schema_ptr: jsi_ptr) }
-  describe 'initialization' do
+  describe 'basic' do
+    let(:jsi_document) do
+      YAML.load(<<~YAML
+        properties:
+          properties:
+            additionalProperties:
+              "$ref": "#"
+          additionalProperties:
+            "$ref": "#"
+          "$ref": {}
+        YAML
+      )
+    end
+    let(:jsi_ptr) { JSI::JSON::Pointer[] }
+    let(:metaschema_root_ptr) { JSI::JSON::Pointer[] }
+    let(:root_schema_ptr) { JSI::JSON::Pointer[] }
+    let(:subject) do
+      JSI::MetaschemaNode.new(jsi_document,
+        jsi_ptr: jsi_ptr,
+        metaschema_root_ptr: jsi_ptr,
+        root_schema_ptr: jsi_ptr,
+      )
+    end
     it 'initializes' do
       subject
     end
