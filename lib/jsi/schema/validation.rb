b5cada69ed90ad8b9cccb0e119916b7ed0d73b9c
Ethan <ethan@unth.net>
2020-05-30 02:14:58 -0700

    note Schema::Validation categories where autoloads will be

--- lib/jsi/schema/validation.rb
+++ lib/jsi/schema/validation.rb
@@ -3,5 +3,23 @@
 module JSI
   module Schema::Validation
     autoload :Core, 'jsi/schema/validation/core'
+
+    # ref application
+
+    # inplace subschema application
+
+    # child subschema application
+
+    # property names subschema application
+
+    # any type validation
+
+    # object validation
+
+    # array validation
+
+    # string validation
+
+    # numeric validation
   end
 end
