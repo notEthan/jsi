a98a88ff73735fd0aeae79bd5d6319114729c98f
Ethan <ethan@unth.net>
2020-08-22 23:46:39 -0700

    test refactor metaschema_node_test #assert_metaschema_behaves

--- test/metaschema_node_test.rb
+++ test/metaschema_node_test.rb
@@ -9,6 +9,13 @@ describe JSI::MetaschemaNode do
   end
   let(:metaschema) { metaschema_root_ptr.evaluate(root_node) }
 
+  def assert_metaschema_behaves
+    assert_is_a(metaschema.jsi_schema_module, metaschema)
+    assert_is_a(metaschema.properties['properties'].jsi_schema_module, metaschema.properties)
+    assert_is_a(metaschema.jsi_schema_module, metaschema.properties['properties'])
+    assert_is_a(metaschema.jsi_schema_module, metaschema.properties['properties'].additionalProperties)
+  end
+
   describe 'basic' do
     let(:jsi_document) do
       YAML.load(<<~YAML
@@ -25,10 +32,7 @@ describe JSI::MetaschemaNode do
     let(:metaschema_root_ptr) { JSI::JSON::Pointer[] }
     let(:root_schema_ptr) { JSI::JSON::Pointer[] }
     it 'acts like a metaschema' do
-      assert_is_a(metaschema.jsi_schema_module, metaschema)
-      assert_is_a(metaschema.properties['properties'].jsi_schema_module, metaschema.properties)
-      assert_is_a(metaschema.jsi_schema_module, metaschema.properties['properties'])
-      assert_is_a(metaschema.jsi_schema_module, metaschema.properties['properties'].additionalProperties)
+      assert_metaschema_behaves
     end
   end
   describe 'json schema draft' do
