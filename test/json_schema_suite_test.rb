3dcab82a28986b4fd3c3ac1e38820b6e26c2feb0
Ethan <ethan@unth.net>
2020-08-27 16:13:59 -0700

    test json_schema_suite_test resets the registry each test, as its URIs collide across spec versions

--- test/json_schema_suite_test.rb
+++ test/json_schema_suite_test.rb
@@ -16,6 +16,11 @@ describe 'JSON Schema Test Suite' do
             describe(subpath.join('/')) do
               JSONSchemaTestSchema.new_jsi(::JSON.parse(path.read)).map do |tests_desc|
                 describe(tests_desc.description) do
+                  around do |test|
+                    registry_before = JSI.schema_registry.dup
+                    test.call
+                    JSI.send(:instance_variable_set, :@schema_registry, registry_before)
+                  end
                   let(:schema) do
                     metaschema.new_schema(tests_desc.jsi_instance['schema'])
                   end
