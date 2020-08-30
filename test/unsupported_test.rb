eca95936a0f242bfe1d89f96f1322abc7847afaf
Ethan <ethan@unth.net>
2020-08-29 21:12:38 -0700

    init test/unsupported_test.rb

---
+++ test/unsupported_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+# the behavior described in these tests is not officially supported, but is not expected to break.
+
+describe 'unsupported behavior' do
+end
