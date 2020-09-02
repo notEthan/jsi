85d6d8dd0101dc05f945378fe45e4e28d4d4a41d
Ethan <ethan@unth.net>
2020-09-01 18:18:57 -0700

    dev Schema::Validation::Draft07 validation modules

--- lib/jsi/schema/validation/draft07.rb
+++ lib/jsi/schema/validation/draft07.rb
@@ -2,5 +2,36 @@
 
 module JSI
   module Schema::Validation::Draft07
+    include Schema::Validation::Core
+
+    include Schema::Validation::Ref
+
+    include Schema::Validation::Type
+    include Schema::Validation::Enum
+    include Schema::Validation::Const
+
+    include Schema::Validation::MultipleOf
+    include Schema::Validation::MinMax
+
+    include Schema::Validation::StringLength
+    include Schema::Validation::Pattern
+
+    include Schema::Validation::Items
+    include Schema::Validation::ArrayLength
+    include Schema::Validation::UniqueItems
+    include Schema::Validation::Contains
+
+    include Schema::Validation::MinMaxProperties
+    include Schema::Validation::Required
+
+    include Schema::Validation::Properties
+    include Schema::Validation::Dependencies
+    include Schema::Validation::PropertyNames
+
+    include Schema::Validation::IfThenElse
+    include Schema::Validation::AllOf
+    include Schema::Validation::AnyOf
+    include Schema::Validation::OneOf
+    include Schema::Validation::Not
   end
 end
