9732019480fbf8eb913534e0837456d55b2d968c
Ethan <ethan@unth.net>
2020-12-02 02:51:07 -0800

    init Schema::Application

--- lib/jsi/schema.rb
+++ lib/jsi/schema.rb
@@ -6,6 +6,8 @@ module JSI
   # the content of an instance which is a JSI::Schema (referred to in this context as schema_content) is
   # expected to be a Hash (JSON object) or a Boolean.
   module Schema
+    autoload :Application, 'jsi/schema/application'
+
     autoload :SchemaAncestorNode, 'jsi/schema/schema_ancestor_node'
 
     autoload :Draft04, 'jsi/schema/draft04'
