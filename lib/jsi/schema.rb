197c1c46d850c1ff30b6fbd3acda69d9fb2bb7f0
Ethan <ethan@unth.net>
2020-12-03 03:43:12 -0800

    refactor mv Schema#match_to_instance to Schema::Application::InplaceApplication

--- lib/jsi/schema.rb
+++ lib/jsi/schema.rb
@@ -263,19 +263,6 @@ module JSI
       end
     end
 
-    # checks this schema for applicators ($ref, allOf, etc.) which should be applied to the given instance.
-    # returns these as a Set of {JSI::Schema}s.
-    #
-    # the returned set will contain this schema itself, unless this schema contains a $ref keyword.
-    #
-    # @param other_instance [Object] the instance to check any applicators against
-    # @return [Set<JSI::Schema>] matched applicator schemas
-    def match_to_instance(other_instance)
-      jsi_ptr.schema_match_ptrs_to_instance(jsi_document, other_instance).map do |ptr|
-        resource_root_subschema(ptr)
-      end.to_set
-    end
-
     # returns a set of subschemas of this schema for the given property name, from keywords
     #   `properties`, `patternProperties`, and `additionalProperties`.
     #
--- lib/jsi/schema/application/inplace_application.rb
+++ lib/jsi/schema/application/inplace_application.rb
@@ -2,5 +2,17 @@
 
 module JSI
   module Schema::Application::InplaceApplication
+    # checks this schema for applicators ($ref, allOf, etc.) which should be applied to the given instance.
+    # returns these as a Set of {JSI::Schema}s.
+    #
+    # the returned set will contain this schema itself, unless this schema contains a $ref keyword.
+    #
+    # @param other_instance [Object] the instance to check any applicators against
+    # @return [Set<JSI::Schema>] matched applicator schemas
+    def match_to_instance(other_instance)
+      jsi_ptr.schema_match_ptrs_to_instance(jsi_document, other_instance).map do |ptr|
+        resource_root_subschema(ptr)
+      end.to_set
+    end
   end
 end
