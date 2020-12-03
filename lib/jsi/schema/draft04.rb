fc378863ac87c058b92c21197a607a10a0e662dc
Ethan <ethan@unth.net>
2020-12-03 02:31:46 -0800

    draft04 draft06 and test metaschemas include Schema::Application::InplaceApplication, Schema::Application::ChildApplication

--- lib/jsi/schema/draft04.rb
+++ lib/jsi/schema/draft04.rb
@@ -4,6 +4,8 @@ module JSI
   module Schema
     module Draft04
       include Schema
+      include Schema::Application::InplaceApplication
+      include Schema::Application::ChildApplication
     end
   end
 end
--- lib/jsi/schema/draft06.rb
+++ lib/jsi/schema/draft06.rb
@@ -4,6 +4,8 @@ module JSI
   module Schema
     module Draft06
       include Schema
+      include Schema::Application::InplaceApplication
+      include Schema::Application::ChildApplication
     end
   end
 end
