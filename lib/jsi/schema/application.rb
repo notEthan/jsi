33273e7f938d7d1880ccabc6aa6ac2757233f40c
Ethan <ethan@unth.net>
2020-12-02 23:41:04 -0800

    init Schema::Application::ChildApplication, Schema::Application::InplaceApplication

--- lib/jsi/schema/application.rb
+++ lib/jsi/schema/application.rb
@@ -2,5 +2,7 @@
 
 module JSI
   module Schema::Application
+    autoload :InplaceApplication, 'jsi/schema/application/inplace_application'
+    autoload :ChildApplication, 'jsi/schema/application/child_application'
   end
 end
