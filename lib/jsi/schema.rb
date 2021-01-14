423c539942e9d47d1864143843cf54f1ad629668
Ethan <ethan@unth.net>
2021-01-13 21:18:20 -0800

    dev use a Schema::Ref to locate schemas' $schema from the registry in JSI::Schema.new_schema; remove supported_metaschemas

--- lib/jsi/schema.rb
+++ lib/jsi/schema.rb
@@ -127,14 +127,6 @@ module JSI
         JSI::JSONSchemaOrgDraft06.schema
       end
 
-      # @return [Array<JSI::Schema>] supported metaschemas
-      def supported_metaschemas
-        [
-          JSI::JSONSchemaOrgDraft04.schema,
-          JSI::JSONSchemaOrgDraft06.schema,
-        ]
-      end
-
       # instantiates a given schema object as a JSI::Schema.
       #
       # schemas are instantiated according to their '$schema' property if specified. otherwise their schema
@@ -155,9 +147,9 @@ module JSI
         elsif schema_object.respond_to?(:to_hash)
           schema_object = JSI.deep_stringify_symbol_keys(schema_object)
           if schema_object.key?('$schema') && schema_object['$schema'].respond_to?(:to_str)
-            metaschema = supported_metaschemas.detect { |ms| schema_object['$schema'] == ms['$id'] || schema_object['$schema'] == ms['id'] }
-            unless metaschema
-              raise(NotImplementedError, "metaschema not supported: #{schema_object['$schema']}")
+            metaschema = Schema::Ref.new(schema_object['$schema']).deref_schema
+            unless metaschema.is_a?(Schema) && metaschema.describes_schema?
+              raise(Schema::ReferenceError, "given schema_object contains a $schema but the resource it identifies does not describe a schema")
             end
             metaschema.new_schema(schema_object, base_uri: base_uri)
           else
