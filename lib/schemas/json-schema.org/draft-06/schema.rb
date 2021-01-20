974ef0128fd6c2c18f79668c748ca2feec4676ac
Ethan <ethan@unth.net>
2021-01-20 10:22:18 -0800

    doc modules JSONSchemaOrgDraft04, JSONSchemaOrgDraft06

--- lib/schemas/json-schema.org/draft-06/schema.rb
+++ lib/schemas/json-schema.org/draft-06/schema.rb
@@ -6,4 +6,8 @@ module JSI
   JSONSchemaOrgDraft06 = MetaschemaNode.new(schema_content,
     metaschema_instance_modules: Set[JSI::Schema::Draft06],
   ).tap(&:register_schema).jsi_schema_module
+
+  # the JSI schema module for http://json-schema.org/draft-06/schema
+  module JSONSchemaOrgDraft06
+  end
 end
