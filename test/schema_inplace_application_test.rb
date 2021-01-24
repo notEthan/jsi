88736e356e19b112a7bbcd67a6ae6e2178676238
Ethan <ethan@unth.net>
2021-01-24 03:45:43 -0800

    test schema application allOf anyOf oneOf

--- test/schema_inplace_application_test.rb
+++ test/schema_inplace_application_test.rb
@@ -94,4 +94,212 @@ describe 'JSI Schema inplace application' do
       end
     end
   end
+  {
+    draft04: JSI::JSONSchemaOrgDraft04,
+    draft06: JSI::JSONSchemaOrgDraft06,
+  }.each do |name, metaschema|
+    describe "#{name} inplace allOf, anyOf, oneOf application" do
+      let(:metaschema) { metaschema }
+      describe 'allOf' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            allOf:
+              - {}
+              - {}
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.allOf[0].jsi_schema_module, subject)
+          assert_is_a(schema.allOf[1].jsi_schema_module, subject)
+        end
+      end
+      describe 'applicators through allOf' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A: {}
+            allOf:
+              - allOf:
+                  - {}
+              - $ref: "#/definitions/A"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies all' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.allOf[0].jsi_schema_module, subject)
+          assert_is_a(schema.allOf[0].allOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.allOf[1].jsi_schema_module, subject) # $ref
+          assert_is_a(schema.definitions['A'].jsi_schema_module, subject)
+        end
+      end
+      describe 'allOf failing validation' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A:
+                type: integer
+            allOf:
+              - allOf:
+                  - {type: integer}
+                  - {not: {}}
+              - $ref: "#/definitions/A"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'still applies all' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.allOf[0].jsi_schema_module, subject)
+          assert_is_a(schema.allOf[0].allOf[0].jsi_schema_module, subject)
+          assert_is_a(schema.allOf[0].allOf[1].jsi_schema_module, subject)
+          refute_is_a(schema.allOf[1].jsi_schema_module, subject) # $ref
+          assert_is_a(schema.definitions['A'].jsi_schema_module, subject)
+        end
+      end
+      describe 'anyOf' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            anyOf:
+              - {}
+              - {type: integer}
+              - {}
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies the ones that validate' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.anyOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.anyOf[1].jsi_schema_module, subject)
+          assert_is_a(schema.anyOf[2].jsi_schema_module, subject)
+        end
+      end
+      describe 'applicators through anyOf' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A: {}
+              B: {type: string}
+            anyOf:
+              - anyOf:
+                  - {}
+              - $ref: "#/definitions/A"
+              - $ref: "#/definitions/B"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies those that validate' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.anyOf[0].jsi_schema_module, subject)
+          assert_is_a(schema.anyOf[0].anyOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.anyOf[1].jsi_schema_module, subject) # $ref
+          assert_is_a(schema.definitions['A'].jsi_schema_module, subject)
+          refute_is_a(schema.anyOf[2].jsi_schema_module, subject)
+          refute_is_a(schema.definitions['B'].jsi_schema_module, subject)
+        end
+      end
+      describe 'anyOf, all failing validation' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A:
+                type: integer
+            anyOf:
+              - anyOf:
+                  - {type: integer}
+                  - {not: {}}
+              - $ref: "#/definitions/A"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies none' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          refute_is_a(schema.anyOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.anyOf[0].anyOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.anyOf[0].anyOf[1].jsi_schema_module, subject)
+          refute_is_a(schema.anyOf[1].jsi_schema_module, subject) # $ref
+          refute_is_a(schema.definitions['A'].jsi_schema_module, subject)
+        end
+      end
+      describe 'oneOf' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            oneOf:
+              - {not: {}}
+              - {type: integer}
+              - {}
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies the one that validates' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[1].jsi_schema_module, subject)
+          assert_is_a(schema.oneOf[2].jsi_schema_module, subject)
+        end
+      end
+      describe 'applicators through oneOf' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A:
+                oneOf:
+                  - {}
+              B: {type: string}
+            oneOf:
+              - oneOf:
+                  - {}
+                  - {}
+              - $ref: "#/definitions/A"
+              - $ref: "#/definitions/B"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies one' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[0].oneOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[0].oneOf[1].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[1].jsi_schema_module, subject) # $ref
+          assert_is_a(schema.definitions['A'].jsi_schema_module, subject)
+          assert_is_a(schema.definitions['A'].oneOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[2].jsi_schema_module, subject)
+          refute_is_a(schema.definitions['B'].jsi_schema_module, subject)
+        end
+      end
+      describe 'oneOf, all failing validation' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A:
+                type: integer
+            oneOf:
+              - oneOf:
+                  - {type: integer}
+                  - {not: {}}
+              - $ref: "#/definitions/A"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies none' do
+          assert_is_a(schema.jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[0].oneOf[0].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[0].oneOf[1].jsi_schema_module, subject)
+          refute_is_a(schema.oneOf[1].jsi_schema_module, subject) # $ref
+          refute_is_a(schema.definitions['A'].jsi_schema_module, subject)
+        end
+      end
+    end
+  end
 end
