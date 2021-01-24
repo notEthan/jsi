5a990e4cbdd652566607f81b8934d1e532c82051
Ethan <ethan@unth.net>
2021-01-24 01:03:37 -0800

    test $ref schema inplace application (draft04, draft06)

--- test/schema_inplace_application_test.rb
+++ test/schema_inplace_application_test.rb
@@ -3,4 +3,82 @@ require_relative 'test_helper'
 describe 'JSI Schema inplace application' do
   let(:schema) { metaschema.new_schema(schema_content) }
   let(:subject) { schema.new_jsi(instance) }
+  {
+    draft04: JSI::JSONSchemaOrgDraft04,
+    draft06: JSI::JSONSchemaOrgDraft06,
+  }.each do |name, metaschema|
+    describe "#{name} inplace $ref application" do
+      let(:metaschema) { metaschema }
+      describe '$ref' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A: {}
+            $ref: "#/definitions/A"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies $ref, excludes the schema containing $ref' do
+          refute_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.definitions['A'].jsi_schema_module, subject)
+        end
+      end
+      describe '$ref nested' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A:
+                $ref: "#/definitions/B"
+              B:
+                {}
+            $ref: "#/definitions/A"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies final $ref' do
+          refute_is_a(schema.jsi_schema_module, subject)
+          refute_is_a(schema.definitions['A'].jsi_schema_module, subject)
+          assert_is_a(schema.definitions['B'].jsi_schema_module, subject)
+        end
+      end
+      describe '$ref sibling applicators' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A: {}
+            $ref: "#/definitions/A"
+            allOf:
+              - {}
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'ignores' do
+          refute_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.definitions['A'].jsi_schema_module, subject)
+          refute_is_a(schema.allOf[0].jsi_schema_module, subject)
+        end
+      end
+      describe 'applicators through $ref target' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            definitions:
+              A:
+                allOf:
+                  - {}
+            $ref: "#/definitions/A"
+            YAML
+          )
+        end
+        let(:instance) { {} }
+        it 'applies' do
+          refute_is_a(schema.jsi_schema_module, subject)
+          assert_is_a(schema.definitions['A'].jsi_schema_module, subject)
+          assert_is_a(schema.definitions['A'].allOf[0].jsi_schema_module, subject)
+        end
+      end
+    end
+  end
 end
