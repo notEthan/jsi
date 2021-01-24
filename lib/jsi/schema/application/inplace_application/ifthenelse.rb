839972d8a7d705a17e19ff7abd36b10755502892
Ethan <ethan@unth.net>
2021-01-23 21:29:52 -0800

    Schema::Application::InplaceApplication::IfThenElse #internal_applicate_ifthenelse

--- lib/jsi/schema/application/inplace_application/ifthenelse.rb
+++ lib/jsi/schema/application/inplace_application/ifthenelse.rb
@@ -2,5 +2,19 @@
 
 module JSI
   module Schema::Application::InplaceApplication::IfThenElse
+    # @private
+    def internal_applicate_ifthenelse(instance, visited_refs, &block)
+      if schema_content.key?('if')
+        if subschema(['if']).instance_valid?(instance)
+          if schema_content.key?('then')
+            subschema(['then']).each_inplace_applicator_schema(instance, visited_refs: visited_refs, &block)
+          end
+        else
+          if schema_content.key?('else')
+            subschema(['else']).each_inplace_applicator_schema(instance, visited_refs: visited_refs, &block)
+          end
+        end
+      end
+    end
   end
 end
