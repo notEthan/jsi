a09b5561ca49bc4b62bfa159d8cf12bde7b9e214
Ethan <ethan@unth.net>
2021-01-23 21:29:52 -0800

    Schema::Cxt::InplaceApplication#inplace_subschema_applicate

--- lib/jsi/schema/cxt/inplace_application.rb
+++ lib/jsi/schema/cxt/inplace_application.rb
@@ -8,6 +8,14 @@ module JSI
     ))
 
     class InplaceApplication
+      # @param subschema_ptr [Ptr, #to_ary]
+      def inplace_subschema_applicate(subschema_ptr)
+        schema.subschema(subschema_ptr).each_inplace_applicator_schema(
+          instance,
+          visited_refs: visited_refs,
+          &block
+        )
+      end
     end
   end
 end
