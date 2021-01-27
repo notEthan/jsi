bd5d6b3e21f8d799f1a7a76c1cf692589e630156
Ethan <ethan@unth.net>
2021-01-26 19:29:06 -0800

    Schema::Application::InplaceApplication::Draft07 include InplaceApplication keyword modules

--- lib/jsi/schema/application/inplace_application/draft07.rb
+++ lib/jsi/schema/application/inplace_application/draft07.rb
@@ -3,5 +3,9 @@
 module JSI
   module Schema::Application::InplaceApplication::Draft07
     include Schema::Application::InplaceApplication
+    include Schema::Application::InplaceApplication::Ref
+    include Schema::Application::InplaceApplication::Dependencies
+    include Schema::Application::InplaceApplication::IfThenElse
+    include Schema::Application::InplaceApplication::SomeOf
   end
 end
