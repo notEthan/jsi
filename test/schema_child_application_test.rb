f3b4ab2c5cca0c34f789ac9a044f50394eb59d0e
Ethan <ethan@unth.net>
2021-01-26 22:03:22 -0800

    test more items, additionalItems in schema_child_application_test

--- test/schema_child_application_test.rb
+++ test/schema_child_application_test.rb
@@ -7,7 +7,7 @@ describe 'JSI Schema child application' do
     draft04: JSI::JSONSchemaOrgDraft04,
     draft06: JSI::JSONSchemaOrgDraft06,
   }.each do |name, metaschema|
-    describe "#{name} child items application" do
+    describe "#{name} child items / additionalItems application" do
       let(:metaschema) { metaschema }
       describe 'items' do
         let(:schema_content) do
@@ -22,6 +22,54 @@ describe 'JSI Schema child application' do
             schema.items,
           ], subject[0].jsi_schemas)
           assert_is_a(schema.items.jsi_schema_module, subject[0])
+        end
+      end
+      describe 'items array' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            items: [{}]
+            YAML
+          )
+        end
+        let(:instance) { [{}, {}] }
+        it 'applies corresponding items' do
+          assert_equal(Set[
+            schema.items[0],
+          ], subject[0].jsi_schemas)
+          assert_is_a(schema.items[0].jsi_schema_module, subject[0])
+          refute_is_a(schema.items[0].jsi_schema_module, subject[1])
+        end
+      end
+      describe 'additionalItems' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            items: [{}]
+            additionalItems: {}
+            YAML
+          )
+        end
+        let(:instance) { [{}, {}] }
+        it 'applies items, additionalItems' do
+          assert_equal(Set[
+            schema.items[0],
+          ], subject[0].jsi_schemas)
+          assert_equal(Set[
+            schema.additionalItems,
+          ], subject[1].jsi_schemas)
+          assert_is_a(schema.items[0].jsi_schema_module, subject[0])
+          assert_is_a(schema.additionalItems.jsi_schema_module, subject[1])
+        end
+      end
+      describe 'additionalItems without items' do
+        let(:schema_content) do
+          YAML.load(<<~YAML
+            additionalItems: {}
+            YAML
+          )
+        end
+        let(:instance) { [{}] }
+        it 'applies none' do
+          refute_is_a(schema.additionalItems.jsi_schema_module, subject[0])
         end
       end
     end
