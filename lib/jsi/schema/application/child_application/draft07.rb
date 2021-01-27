a1ccfc3a8a568faac1d20368c5456a86c5871ccb
Ethan <ethan@unth.net>
2021-01-26 19:50:15 -0800

    Schema::Application::ChildApplication::Draft07 include ChildApplication keyword modules

--- lib/jsi/schema/application/child_application/draft07.rb
+++ lib/jsi/schema/application/child_application/draft07.rb
@@ -3,5 +3,8 @@
 module JSI
   module Schema::Application::ChildApplication::Draft07
     include Schema::Application::ChildApplication
+    include Schema::Application::ChildApplication::Items
+    include Schema::Application::ChildApplication::Contains
+    include Schema::Application::ChildApplication::Properties
   end
 end
