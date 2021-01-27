b11ac425c384a0714782b63c0d9c2d9d544e964f
Ethan <ethan@unth.net>
2021-01-26 19:29:06 -0800

    InplaceApplication Draft04, Draft06 include the keyword modules for ref, *Of

--- lib/jsi/schema/application/inplace_application/draft04.rb
+++ lib/jsi/schema/application/inplace_application/draft04.rb
@@ -3,5 +3,7 @@
 module JSI
   module Schema::Application::InplaceApplication::Draft04
     include Schema::Application::InplaceApplication
+    include Schema::Application::InplaceApplication::Ref
+    include Schema::Application::InplaceApplication::SomeOf
   end
 end
--- lib/jsi/schema/application/inplace_application/draft06.rb
+++ lib/jsi/schema/application/inplace_application/draft06.rb
@@ -3,5 +3,7 @@
 module JSI
   module Schema::Application::InplaceApplication::Draft06
     include Schema::Application::InplaceApplication
+    include Schema::Application::InplaceApplication::Ref
+    include Schema::Application::InplaceApplication::SomeOf
   end
 end
