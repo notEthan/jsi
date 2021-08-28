5929b1a18cd6e41f29190983eebb646f66a7c509
Ethan <ethan@unth.net>
2021-08-28 02:50:01 -0700

    remove deprecated alias JSI::JSON::Pointer
    moved and deprecated in https://github.com/notEthan/jsi/pull/160

--- lib/jsi.rb
+++ lib/jsi.rb
@@ -30,13 +30,6 @@ module JSI
   SCHEMAS_PATH = RESOURCES_PATH.join('schemas')
 
   autoload :Ptr, 'jsi/ptr'
-
-  # @private
-  # @deprecated
-  module JSON
-    Pointer = Ptr
-  end
-
   autoload :PathedNode, 'jsi/pathed_node'
   autoload :Typelike, 'jsi/typelike_modules'
   autoload :Hashlike, 'jsi/typelike_modules'
