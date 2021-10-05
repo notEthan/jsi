83c88d5a84bf5a79ce24ce82c1800263bc4e5d12
Ethan <ethan@unth.net>
2021-10-05 14:48:39 -0700

    add missing frozen_string_literal

--- lib/jsi/schema/schema_ancestor_node.rb
+++ lib/jsi/schema/schema_ancestor_node.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module JSI
   # a node in a document which may contain a schema somewhere within is extended with SchemaAncestorNode, for
   # tracking things necessary for a schema to function correctly
