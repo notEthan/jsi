b7ce16fd110e05a7601bd199990d5b60df0251d6
Ethan <ethan@unth.net>
2021-10-15 18:47:30 -0700

    test Schema::Ref anchor across nested resources

--- test/schema_ref_test.rb
+++ test/schema_ref_test.rb
@@ -29,5 +29,75 @@ describe JSI::Schema::Ref do
         assert_match(/found multiple schemas for plain name fragment/, err.message)
       end
     end
+
+    describe 'same anchor, parent and child resources' do
+      let(:schema) do
+        JSI::JSONSchemaOrgDraft06.new_schema(JSON.parse(%q({
+          "$id": "http://jsi/schema_ref/m6xe",
+          "definitions": {
+            "ref": {"$ref": "#X"},
+            "child": {
+              "$id": "#X",
+              "definitions": {
+                "ref": {"$ref": "#X"},
+                "rel": {
+                  "$id": "svl3",
+                  "definitions": {
+                    "ref": {"$ref": "#X"},
+                    "x": {"$id": "#X"}
+                  }
+                }
+              }
+            }
+          }
+        })))
+      end
+
+      it 'resolves' do
+        assert_equal(
+          JSI::SchemaSet[schema.definitions['child']],
+          schema.definitions['ref'].new_jsi({}).jsi_schemas
+        )
+        assert_equal(
+          JSI::SchemaSet[schema.definitions['child']],
+          schema.definitions['child'].definitions['ref'].new_jsi({}).jsi_schemas
+        )
+        assert_equal(
+          JSI::SchemaSet[schema.definitions['child'].definitions['rel'].definitions['x']],
+          schema.definitions['child'].definitions['rel'].definitions['ref'].new_jsi({}).jsi_schemas
+        )
+      end
+    end
+
+    describe 'anchor alongside absolute uri in child' do
+      let(:schema) do
+        JSI::JSONSchemaOrgDraft06.new_schema(JSON.parse(%q({
+          "$id": "http://jsi/schema_ref/os0e",
+          "definitions": {
+            "ref": {"$ref": "#X"},
+            "child": {
+              "$id": "#X",
+              "definitions": {
+                "ref": {"$ref": "#X"},
+                "x": {
+                  "$id": "x99u#X"
+                }
+              }
+            }
+          }
+        })))
+      end
+
+      it 'resolves' do
+        assert_equal(
+          JSI::SchemaSet[schema.definitions['child']],
+          schema.definitions['ref'].new_jsi({}).jsi_schemas
+        )
+        assert_equal(
+          JSI::SchemaSet[schema.definitions['child']],
+          schema.definitions['child'].definitions['ref'].new_jsi({}).jsi_schemas
+        )
+      end
+    end
   end
 end
