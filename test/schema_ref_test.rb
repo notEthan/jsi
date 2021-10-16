9611a6c77cc87ed319d556f61aa4cc8fa5e6650b
Ethan <ethan@unth.net>
2021-10-15 18:47:30 -0700

    test Schema::Ref with a normal anchor and ambiguous anchors

--- test/schema_ref_test.rb
+++ test/schema_ref_test.rb
@@ -1,4 +1,33 @@
 require_relative 'test_helper'
 
 describe JSI::Schema::Ref do
+  describe 'anchors' do
+    describe 'no anchor' do
+      let(:schema) do
+        JSI::JSONSchemaOrgDraft06.new_schema({"$ref" => "#no"})
+      end
+
+      it 'finds none' do
+        err = assert_raises(JSI::Schema::ReferenceError) { schema.new_jsi({}) }
+        assert_match(/could not find schema by fragment/, err.message)
+      end
+    end
+
+    describe 'conflicting siblings' do
+      let(:schema) do
+        JSI::JSONSchemaOrgDraft06.new_schema(YAML.safe_load(%q({
+          "definitions": {
+            "sibling1": {"$id": "#collide"},
+            "sibling2": {"$id": "#collide"},
+            "ref": {"$ref": "#collide"}
+          }
+        })))
+      end
+
+      it 'finds a collision' do
+        err = assert_raises(JSI::Schema::ReferenceError) { schema.definitions['ref'].new_jsi({}) }
+        assert_match(/found multiple schemas for plain name fragment/, err.message)
+      end
+    end
+  end
 end
