f2e96f8636e3971b88e27b4fc5ca59a849f666e2
Ethan <ethan@unth.net>
2021-10-24 02:56:31 -0700

    doc Util.ycomb

--- lib/jsi/util.rb
+++ lib/jsi/util.rb
@@ -81,12 +81,16 @@ module JSI
     # this is the Y-combinator, which allows anonymous recursive functions. for a simple example,
     # to define a recursive function to return the length of an array:
     #
-    #    length = ycomb do |len|
-    #      proc { |list| list == [] ? 0 : 1 + len.call(list[1..-1]) }
-    #    end
+    #     length = ycomb do |len|
+    #       proc { |list| list == [] ? 0 : 1 + len.call(list[1..-1]) }
+    #     end
     #
-    # see https://secure.wikimedia.org/wikipedia/en/wiki/Fixed_point_combinator#Y_combinator
-    # and chapter 9 of the little schemer, available as the sample chapter at http://www.ccs.neu.edu/home/matthias/BTLS/
+    #     length.call([0])
+    #     # => 1
+    #
+    # see https://en.wikipedia.org/wiki/Fixed-point_combinator#Y_combinator
+    # and chapter 9 of the little schemer, available as the sample chapter at
+    # https://felleisen.org/matthias/BTLS-index.html
     def ycomb
       proc { |f| f.call(f) }.call(proc { |f| yield proc { |*x| f.call(f).call(*x) } })
     end
