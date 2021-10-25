1d4696c65c80d25edd937d731a191154f7c7af82
Ethan <ethan@unth.net>
2021-10-25 02:30:31 -0700

    refactor SimpleWrap instantiate with new_schema_module

--- lib/jsi/simple_wrap.rb
+++ lib/jsi/simple_wrap.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module JSI
-  SimpleWrap = JSI::JSONSchemaOrgDraft06.new_schema({
+  SimpleWrap = JSI::JSONSchemaOrgDraft06.new_schema_module({
     "additionalProperties": {"$ref": "#"},
     "items": {"$ref": "#"}
-  }).jsi_schema_module
+  })
 
   # SimpleWrap is a JSI schema module which recursively wraps nested structures
   module SimpleWrap
