29f033dc3d28f8f28c96f978fcbd5bdd667598c9
Ethan <ethan@unth.net>
2021-10-26 04:35:14 -0700

    SimpleWrap drops the draft6 spec, gets its own custom metaschema + implementation

--- lib/jsi/simple_wrap.rb
+++ lib/jsi/simple_wrap.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
 module JSI
-  SimpleWrap = JSI::JSONSchemaOrgDraft06.new_schema_module({
-    "additionalProperties": {"$ref": "#"},
-    "items": {"$ref": "#"}
-  })
+  module SimpleWrapImplementation
+    include Schema
+    include Schema::Application::ChildApplication
+    include Schema::Application::InplaceApplication
+    include Schema::Validation::Core
+
+    def internal_child_applicate_keywords(token, instance)
+      yield self
+    end
+
+    def internal_inplace_applicate_keywords(instance, visited_refs)
+      yield self
+    end
+
+    def internal_validate_keywords(result_builder)
+    end
+  end
+
+  simple_wrap_metaschema = MetaschemaNode.new(nil, metaschema_instance_modules: [JSI::SimpleWrapImplementation])
+  SimpleWrap = simple_wrap_metaschema.new_schema_module({})
 
   # SimpleWrap is a JSI schema module which recursively wraps nested structures
   module SimpleWrap
