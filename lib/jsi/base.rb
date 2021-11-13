ca1c2ec49e382cb76ef5f910184f4619cdf68e56
Ethan <ethan@unth.net>
2021-11-12 23:00:00 -0800

    Base#jsi_fingerprint key fix

--- lib/jsi/base.rb
+++ lib/jsi/base.rb
@@ -549,7 +549,7 @@ module JSI
         jsi_document: jsi_document,
         jsi_ptr: jsi_ptr,
         # for instances in documents with schemas:
-        jsi_schema_base_uri: jsi_resource_ancestor_uri,
+        jsi_resource_ancestor_uri: jsi_resource_ancestor_uri,
         # only defined for JSI::Schema instances:
         jsi_schema_instance_modules: is_a?(Schema) ? jsi_schema_instance_modules : nil,
       }
