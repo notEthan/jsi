e5a6012cf1327a43528bbf9cba4a59779e9daa04
Ethan <ethan@unth.net>
2021-11-30 12:44:20 -0800

    initial Schema::Element dev initialize, actions

--- lib/jsi/schema/element.rb
+++ lib/jsi/schema/element.rb
@@ -3,6 +3,26 @@
 module JSI
   module Schema
     class Element
+      # @yield [Schema::Element] self
+      def initialize
+        @actions = Hash.new(Util::EMPTY_ARY)
+
+        yield(self)
+
+        @actions.freeze
+        freeze
+      end
+
+      # @param name [Symbol]
+      # @yield perform the action
+      # @return [void]
+      def add_action(name, &block)
+        raise(TypeError) unless name.is_a?(Symbol)
+
+        @actions[name] = @actions[name].dup.push(block).freeze
+
+        nil
+      end
     end
   end
 end
