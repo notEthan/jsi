4e3774cde102501c7d3170703f0997acfad537d4
Ethan <ethan@unth.net>
2021-11-30 12:43:07 -0800

    Schema::Vocabulary #initialize with elements

--- lib/jsi/schema/vocabulary.rb
+++ lib/jsi/schema/vocabulary.rb
@@ -3,6 +3,16 @@
 module JSI
   module Schema
     class Vocabulary
+      # @param elements [Enumerable<Schema::Element>]
+      def initialize(elements: )
+        raise(TypeError, "elements: #{elements}") unless elements.all? { |e| e.is_a?(Schema::Element) }
+        @elements = Set.new(elements).freeze
+
+        freeze
+      end
+
+      # @return [Set<Schema::Element>]
+      attr_reader(:elements)
     end
   end
 end
