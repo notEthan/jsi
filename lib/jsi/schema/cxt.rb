f9a60b583d2088b1d0a3f00a551f7e4f8f6fafea
Ethan <ethan@unth.net>
2021-12-10 22:50:00 -0800

    Schema::Cxt Util::AttrStruct['schema'], #subschema #schema_content #keyword?

--- lib/jsi/schema/cxt.rb
+++ lib/jsi/schema/cxt.rb
@@ -2,7 +2,22 @@
 
 module JSI
   module Schema
+    Cxt = Util::AttrStruct[*%w(
+      schema
+    )]
+
     class Cxt
+      def subschema(subptr)
+        schema.subschema(subptr)
+      end
+
+      def schema_content
+        schema.jsi_node_content
+      end
+
+      def keyword?(keyword)
+        schema.keyword?(keyword)
+      end
     end
   end
 end
