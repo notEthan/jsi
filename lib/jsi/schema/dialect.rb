a6704e0755b412125de2dc9a62769a2a3342acf3
Ethan <ethan@unth.net>
2021-12-13 02:56:37 -0800

    Schema::Element specify dependencies between elements with #required_before_elements, #depends_on_elements

--- lib/jsi/schema/dialect.rb
+++ lib/jsi/schema/dialect.rb
@@ -22,10 +22,32 @@ module JSI
       # @param cxt [Schema::Cxt] the `self` of the action
       # @return given `cxt`
       def invoke(action_name, cxt)
+        invoked_elements = Set[]
+        uninvoked_elements = elements.dup
+
+        # key element depends on each element of its value
+        dependencies = Hash.new { |h, k| h[k] = Set[] }
         elements.each do |element|
-          element.actions[action_name].each do |action|
+          element.select_elements_self_is_required_before(elements).each do |required_before_element|
+            # element will be invoked before required_before_element
+            dependencies[required_before_element] << element
+          end
+
+          element.select_elements_self_depends_on(elements).each do |depends_on_element|
+            # element will be invoked after depends_on_element
+            dependencies[element] << depends_on_element
+          end
+        end
+
+        until uninvoked_elements.empty?
+          invoke_element = uninvoked_elements.detect do |element|
+            dependencies[element].all? { |req_el| invoked_elements.include?(req_el) }
+          end || raise(Bug)
+          invoke_element.actions[action_name].each do |action|
             cxt.instance_exec(&action)
           end
+          invoked_elements.add(invoke_element)
+          uninvoked_elements.delete(invoke_element)
         end
 
         cxt
