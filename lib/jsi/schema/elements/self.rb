fc4e517df492d231e9cfe12edfb9efa32fa03b8f
Ethan <ethan@unth.net>
2021-12-12 20:28:14 -0800

    Schema::Elements::SELF action :inplace_applicate

--- lib/jsi/schema/elements/self.rb
+++ lib/jsi/schema/elements/self.rb
@@ -4,6 +4,9 @@ module JSI
   module Schema::Elements
     SELF = element_map do
       Schema::Element.new do |element|
+        element.add_action(:inplace_applicate) do
+          cxt_yield(schema)
+        end
       end
     end
   end
