3f1d7045e31522f42c28277b93d0c6000ec787c1
Ethan <ethan@unth.net>
2021-12-19 01:44:29 -0800

    Schema::Dialect #initialize with vocabularies

--- lib/jsi/schema/dialect.rb
+++ lib/jsi/schema/dialect.rb
@@ -3,6 +3,15 @@
 module JSI
   module Schema
     class Dialect
+      # @param vocabularies [Enumerable<Schema::Vocabulary>]
+      def initialize(vocabularies: )
+        @vocabularies = Set.new(vocabularies).freeze
+
+        freeze
+      end
+
+      # @return [Set<Schema::Vocabulary>]
+      attr_reader(:vocabularies)
     end
   end
 end
