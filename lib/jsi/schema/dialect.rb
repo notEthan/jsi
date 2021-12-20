5e7c4a00823d54011c8f1c5466fb4fbc04eba3a1
Ethan <ethan@unth.net>
2021-12-19 18:49:27 -0800

    Schema::Dialect#elements

--- lib/jsi/schema/dialect.rb
+++ lib/jsi/schema/dialect.rb
@@ -6,12 +6,16 @@ module JSI
       # @param vocabularies [Enumerable<Schema::Vocabulary>]
       def initialize(vocabularies: )
         @vocabularies = Set.new(vocabularies).freeze
+        @elements = vocabularies.map(&:elements).inject(Set.new, &:merge).freeze
 
         freeze
       end
 
       # @return [Set<Schema::Vocabulary>]
       attr_reader(:vocabularies)
+
+      # @return [Set<Schema::Element>]
+      attr_reader(:elements)
     end
   end
 end
