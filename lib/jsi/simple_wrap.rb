70892c70a54f26be4c9a4a948533fd78c259b36c
Ethan <ethan@unth.net>
2021-12-20 04:15:06 -0800

    SimpleWrap #dialect with initial empty vocabulary

--- lib/jsi/simple_wrap.rb
+++ lib/jsi/simple_wrap.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 module JSI
+  dialect = Schema::Dialect.new(
+    vocabularies: [
+      Schema::Vocabulary.new(elements: [
+      ]),
+    ],
+  )
+
   simple_wrap_implementation = Module.new do
+    define_method(:dialect) do
+      dialect
+    end
+
     def internal_child_applicate_keywords(token, instance)
       yield self
     end
@@ -22,5 +33,6 @@ module JSI
   end
 
   SimpleWrap::Implementation = simple_wrap_implementation
+  SimpleWrap::DIALECT = dialect
   SimpleWrap::METASCHEMA = simple_wrap_metaschema
 end
