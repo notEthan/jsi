2f2171cfdbc0260a0d533280d4f69d097a7e49a0
Ethan <ethan@unth.net>
2021-12-27 20:09:41 -0600

    Schema::Elements::ID action :id

--- lib/jsi/schema/elements/id.rb
+++ lib/jsi/schema/elements/id.rb
@@ -4,6 +4,7 @@ module JSI
   module Schema::Elements
     ID = element_map do |keyword: |
       Schema::Element.new do |element|
+        element.add_action(:id) { cxt_yield(schema_content[keyword]) if keyword_value_str?(keyword) }
       end
     end
   end
