0bc430e0e03a3bd891e028c0e2a6e3305036843c
Ethan <ethan@unth.net>
2022-03-06 05:04:19 -0800

    remove typelike_modules require - redundant with JSI autoloads

--- lib/jsi.rb
+++ lib/jsi.rb
@@ -9,7 +9,6 @@ require "bigdecimal"
 require "addressable/uri"
 
 require "jsi/util"
-require "jsi/typelike_modules"
 
 module JSI
   # generally put in code paths that are not expected to be valid control flow paths.
