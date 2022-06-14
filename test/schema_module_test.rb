c208454b22998d8a955852daf6c0da4cd8d7ab9a
Ethan <ethan@unth.net>
2022-06-13 17:06:15 -0700

    test fix name referring to ancestor nodes as parents
    parent should be the immediate parent; ancestor anywhere at or above

--- test/schema_module_test.rb
+++ test/schema_module_test.rb
@@ -25,7 +25,7 @@ describe 'JSI::SchemaModule' do
     end
   end
   describe '.inspect, .to_s' do
-    it 'shows the name relative to a named parent module' do
+    it 'shows the name relative to a named ancestor schema module' do
       assert_equal(
         'SchemaModuleTestModule.properties (JSI wrapper for Schema Module)',
         SchemaModuleTestModule.properties.inspect
