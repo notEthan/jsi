e209b01fd22c365f1b10727836f8bba3fd07fd0e
Ethan <ethan@unth.net>
2022-06-19 21:05:58 -0700

    rm temp Validation::Result::Builder#result_builder

--- lib/jsi/validation/result.rb
+++ lib/jsi/validation/result.rb
@@ -15,10 +15,6 @@ module JSI
       # @private
       # context to build a Validation::Result
       class Builder
-        def result_builder
-          self
-        end
-
         def instance
           instance_ptr.evaluate(instance_document)
         end
