101b6e848f47a3950a75ad4e823139561218ec40
Ethan <ethan@unth.net>
2022-08-02 11:43:01 -0700

    JSONSchemaDraft202012 set up meta-schema, vocabulary schema documents

--- lib/schemas/json-schema.org/draft/2020-12/schema.rb
+++ lib/schemas/json-schema.org/draft/2020-12/schema.rb
@@ -3,6 +3,12 @@
 module JSI
   dialect = Schema::Draft202012::DIALECT
 
+  path = SCHEMAS_PATH.join('json-schema.org/draft/2020-12')
+  metaschema_document = JSON.parse(path.join('schema.json').read, freeze: true)
+  vocabulary_schema_documents = metaschema_document['allOf'].map do |schema|
+    JSON.parse(path.join(schema['$ref'] + '.json').read, freeze: true)
+  end
+
   module JSONSchemaDraft202012
   end
 end
