6779b46aea5ce0069bb50e4cb9beae2da7f681fd
Ethan <ethan@unth.net>
2022-08-05 14:51:18 -0700

    Schema::Draft202012 vocabulary elements

--- lib/jsi/schema/draft202012.rb
+++ lib/jsi/schema/draft202012.rb
@@ -15,6 +15,34 @@ module JSI
     Vocab::CORE = Schema::Vocabulary.new(
       id: "https://json-schema.org/draft/2020-12/vocab/core",
       elements: [
+        Schema::Elements::SELF[],
+
+        # draft-bhutton-json-schema-01 8.1.1.  The "$schema" Keyword
+        Schema::Elements::XSCHEMA[],
+
+        # draft-bhutton-json-schema-01 8.1.2.  The "$vocabulary" Keyword
+        Schema::Elements::XVOCABULARY[],
+
+        # draft-bhutton-json-schema-01 8.2.1.  The "$id" Keyword
+        Schema::Elements::ID[keyword: '$id', fragment_is_anchor: false],
+
+        # draft-bhutton-json-schema-01 8.2.2.  Defining location-independent identifiers
+        Schema::Elements::ANCHOR[keyword: '$anchor', actions: [:anchor]],
+        Schema::Elements::ANCHOR[keyword: '$dynamicAnchor', actions: [:anchor, :dynamicAnchor]],
+
+        # draft-bhutton-json-schema-01 8.2.3.  Schema References
+
+        # draft-bhutton-json-schema-01 8.2.3.1.  Direct References with "$ref"
+        Schema::Elements::REF[exclusive: false],
+
+        # draft-bhutton-json-schema-01 8.2.3.2.  Dynamic References with "$dynamicRef"
+        Schema::Elements::DYNAMIC_REF[],
+
+        # draft-bhutton-json-schema-01 8.2.4.  Schema Re-Use With "$defs"
+        Schema::Elements::DEFINITIONS[keyword: '$defs'],
+
+        # draft-bhutton-json-schema-01 8.3.  Comments With "$comment"
+        Schema::Elements::COMMENT[],
       ],
     )
 
@@ -28,6 +56,50 @@ module JSI
     Vocab::APPLICATOR = Schema::Vocabulary.new(
       id: "https://json-schema.org/draft/2020-12/vocab/applicator",
       elements: [
+        # draft-bhutton-json-schema-01 10.2.  Keywords for Applying Subschemas in Place
+        # draft-bhutton-json-schema-01 10.2.1.  Keywords for Applying Subschemas With Logic
+
+        # draft-bhutton-json-schema-01 10.2.1.1.  allOf
+        Schema::Elements::ALL_OF[],
+
+        # draft-bhutton-json-schema-01 10.2.1.2.  anyOf
+        Schema::Elements::ANY_OF[],
+
+        # draft-bhutton-json-schema-01 10.2.1.3.  oneOf
+        Schema::Elements::ONE_OF[],
+
+        # draft-bhutton-json-schema-01 10.2.1.4.  not
+        Schema::Elements::NOT[],
+
+        # draft-bhutton-json-schema-01 10.2.2.  Keywords for Applying Subschemas Conditionally
+
+        # draft-bhutton-json-schema-01 10.2.2.1.  if
+        # draft-bhutton-json-schema-01 10.2.2.2.  then
+        # draft-bhutton-json-schema-01 10.2.2.3.  else
+        Schema::Elements::IF_THEN_ELSE[],
+
+        # draft-bhutton-json-schema-01 10.2.2.4.  dependentSchemas
+        Schema::Elements::DEPENDENT_SCHEMAS[],
+
+        # draft-bhutton-json-schema-01 10.3.  Keywords for Applying Subschemas to Child Instances
+        # draft-bhutton-json-schema-01 10.3.1.  Keywords for Applying Subschemas to Arrays
+
+        # draft-bhutton-json-schema-01 10.3.1.1.  prefixItems
+        # draft-bhutton-json-schema-01 10.3.1.2.  items
+        Schema::Elements::ITEMS_PREFIXED[],
+
+        # draft-bhutton-json-schema-01 10.3.1.3.  contains
+        Schema::Elements::CONTAINS_MINMAX[],
+
+        # draft-bhutton-json-schema-01 10.3.2.  Keywords for Applying Subschemas to Objects
+
+        # draft-bhutton-json-schema-01 10.3.2.1.  properties
+        # draft-bhutton-json-schema-01 10.3.2.2.  patternProperties
+        # draft-bhutton-json-schema-01 10.3.2.3.  additionalProperties
+        Schema::Elements::PROPERTIES[],
+
+        # draft-bhutton-json-schema-01 10.3.2.4.  propertyNames
+        Schema::Elements::PROPERTY_NAMES[],
       ],
     )
 
@@ -41,6 +113,11 @@ module JSI
     Vocab::UNEVALUATED = Schema::Vocabulary.new(
       id: "https://json-schema.org/draft/2020-12/vocab/unevaluated",
       elements: [
+        # draft-bhutton-json-schema-01 11.2.  unevaluatedItems
+        Schema::Elements::UNEVALUATED_ITEMS[],
+
+        # draft-bhutton-json-schema-01 11.3.  unevaluatedProperties
+        Schema::Elements::UNEVALUATED_PROPERTIES[],
       ],
     )
 
@@ -54,6 +131,73 @@ module JSI
     Vocab::VALIDATION = Schema::Vocabulary.new(
       id: "https://json-schema.org/draft/2020-12/vocab/validation",
       elements: [
+        # draft-bhutton-json-schema-validation-01 6.1.  Validation Keywords for Any Instance Type
+
+        # draft-bhutton-json-schema-validation-01 6.1.1.  type
+        Schema::Elements::TYPE[],
+
+        # draft-bhutton-json-schema-validation-01 6.1.2.  enum
+        Schema::Elements::ENUM[],
+
+        # draft-bhutton-json-schema-validation-01 6.1.3.  const
+        Schema::Elements::CONST[],
+
+        # draft-bhutton-json-schema-validation-01 6.2.  Validation Keywords for Numeric Instances (number and integer)
+
+        # draft-bhutton-json-schema-validation-01 6.2.1.  multipleOf
+        Schema::Elements::MULTIPLE_OF[],
+
+        # draft-bhutton-json-schema-validation-01 6.2.2.  maximum
+        Schema::Elements::MAXIMUM[],
+
+        # draft-bhutton-json-schema-validation-01 6.2.3.  exclusiveMaximum
+        Schema::Elements::EXCLUSIVE_MAXIMUM[],
+
+        # draft-bhutton-json-schema-validation-01 6.2.4.  minimum
+        Schema::Elements::MINIMUM[],
+
+        # draft-bhutton-json-schema-validation-01 6.2.5.  exclusiveMinimum
+        Schema::Elements::EXCLUSIVE_MINIMUM[],
+
+        # draft-bhutton-json-schema-validation-01 6.3.  Validation Keywords for Strings
+
+        # draft-bhutton-json-schema-validation-01 6.3.1.  maxLength
+        Schema::Elements::MAX_LENGTH[],
+
+        # draft-bhutton-json-schema-validation-01 6.3.2.  minLength
+        Schema::Elements::MIN_LENGTH[],
+
+        # draft-bhutton-json-schema-validation-01 6.3.3.  pattern
+        Schema::Elements::PATTERN[],
+
+        # draft-bhutton-json-schema-validation-01 6.4.  Validation Keywords for Arrays
+
+        # draft-bhutton-json-schema-validation-01 6.4.1.  maxItems
+        Schema::Elements::MAX_ITEMS[],
+
+        # draft-bhutton-json-schema-validation-01 6.4.2.  minItems
+        Schema::Elements::MIN_ITEMS[],
+
+        # draft-bhutton-json-schema-validation-01 6.4.3.  uniqueItems
+        Schema::Elements::UNIQUE_ITEMS[],
+
+        # draft-bhutton-json-schema-validation-01 6.4.4.  maxContains
+        # draft-bhutton-json-schema-validation-01 6.4.5.  minContains
+        # (see Schema::Elements::CONTAINS_MINMAX[] - draft-bhutton-json-schema-01 10.3.1.3.  contains)
+
+        # draft-bhutton-json-schema-validation-01 6.5.  Validation Keywords for Objects
+
+        # draft-bhutton-json-schema-validation-01 6.5.1.  maxProperties
+        Schema::Elements::MAX_PROPERTIES[],
+
+        # draft-bhutton-json-schema-validation-01 6.5.2.  minProperties
+        Schema::Elements::MIN_PROPERTIES[],
+
+        # draft-bhutton-json-schema-validation-01 6.5.3.  required
+        Schema::Elements::REQUIRED[],
+
+        # draft-bhutton-json-schema-validation-01 6.5.4.  dependentRequired
+        Schema::Elements::DEPENDENT_REQUIRED[],
       ],
     )
 
@@ -67,6 +211,8 @@ module JSI
     Vocab::FORMAT_ANNOTATION = Schema::Vocabulary.new(
       id: "https://json-schema.org/draft/2020-12/vocab/format-annotation",
       elements: [
+        # draft-bhutton-json-schema-validation-01 7.2.1.  Format-Annotation Vocabulary
+        Schema::Elements::FORMAT[],
       ],
     )
 
@@ -91,6 +237,14 @@ module JSI
     Vocab::CONTENT = Schema::Vocabulary.new(
       id: "https://json-schema.org/draft/2020-12/vocab/content",
       elements: [
+        # draft-bhutton-json-schema-validation-01 8.3.  contentEncoding
+        Schema::Elements::CONTENT_ENCODING[],
+
+        # draft-bhutton-json-schema-validation-01 8.4.  contentMediaType
+        Schema::Elements::CONTENT_MEDIA_TYPE[],
+
+        # draft-bhutton-json-schema-validation-01 8.5.  contentSchema
+        Schema::Elements::CONTENT_SCHEMA[],
       ],
     )
 
@@ -104,6 +258,22 @@ module JSI
     Vocab::METADATA = Schema::Vocabulary.new(
       id: "https://json-schema.org/draft/2020-12/vocab/meta-data",
       elements: [
+        # draft-bhutton-json-schema-validation-01 9.1.  "title" and "description"
+        Schema::Elements::INFO_STRING[keyword: 'title'],
+        Schema::Elements::INFO_STRING[keyword: 'description'],
+
+        # draft-bhutton-json-schema-validation-01 9.2.  "default"
+        Schema::Elements::DEFAULT[],
+
+        # draft-bhutton-json-schema-validation-01 9.3.  "deprecated"
+        Schema::Elements::INFO_BOOL[keyword: 'deprecated'],
+
+        # draft-bhutton-json-schema-validation-01 9.4.  "readOnly" and "writeOnly"
+        Schema::Elements::INFO_BOOL[keyword: 'readOnly'],
+        Schema::Elements::INFO_BOOL[keyword: 'writeOnly'],
+
+        # draft-bhutton-json-schema-validation-01 9.5.  "examples"
+        Schema::Elements::EXAMPLES[],
       ],
     )
 
