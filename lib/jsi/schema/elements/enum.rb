542cc78227eceb7c630cc8d144b449ed8c635b50
Ethan <ethan@unth.net>
2022-08-11 04:19:09 -0700

    Schema::Elements::ENUM spec comment

--- lib/jsi/schema/elements/enum.rb
+++ lib/jsi/schema/elements/enum.rb
@@ -7,8 +7,7 @@ module JSI
         element.add_action(:validate) do
       if keyword?('enum')
         value = schema_content['enum']
-        # The value of this keyword MUST be an array. This array SHOULD have at least one element.
-        # Elements in the array SHOULD be unique.
+        #> The value of this keyword MUST be an array.
         if value.respond_to?(:to_ary)
           # An instance validates successfully against this keyword if its value is equal to one of the
           # elements in this keyword's array value.
