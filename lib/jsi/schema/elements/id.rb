70e5f7fdde03e9d5e9e15df23ee86ad86c611c74
Ethan <ethan@unth.net>
2022-08-11 03:29:01 -0700

    Schema::Elements::ID action :anchor

--- lib/jsi/schema/elements/id.rb
+++ lib/jsi/schema/elements/id.rb
@@ -5,6 +5,16 @@ module JSI
     ID = element_map do |keyword: , fragment_is_anchor: |
       Schema::Element.new do |element|
         element.add_action(:id) { cxt_yield(schema_content[keyword]) if keyword_value_str?(keyword) }
+
+        if fragment_is_anchor
+          element.add_action(:anchor) do
+            next if !keyword_value_str?(keyword)
+            id_fragment = Util.uri(schema_content[keyword]).fragment
+            if id_fragment && !id_fragment.empty?
+              cxt_yield(id_fragment)
+            end
+          end
+        end
       end
     end
   end
