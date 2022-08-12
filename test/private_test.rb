bfe2a755894868f035f2b5b82619a98c2e54e1d3
Ethan <ethan@unth.net>
2022-08-12 01:35:51 -0700

    init test/private_test.rb

---
+++ test/private_test.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+# tests of private APIs. although private methods are usually tested indirectly, called by public APIs they
+# exist to support, some code paths for development or debugging are not.
