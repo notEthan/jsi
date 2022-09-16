f6e2643fd8a452ceeda1bac14c81ef2ddc0033be
Ethan <ethan@unth.net>
2022-09-16 05:06:25 -0700

    Schema::Elements::UNEVALUATED_PROPERTIES action :subschema

--- lib/jsi/schema/elements/unevaluated_properties.rb
+++ lib/jsi/schema/elements/unevaluated_properties.rb
@@ -5,6 +5,13 @@ module JSI
     # https://json-schema.org/draft/2020-12/draft-bhutton-json-schema-01#name-unevaluatedproperties
     UNEVALUATED_PROPERTIES = element_map do
       Schema::Element.new(keyword: 'unevaluatedProperties') do |element|
+        element.add_action(:subschema) do
+          #> The value of "unevaluatedProperties" MUST be a valid JSON Schema.
+          if keyword?('unevaluatedProperties')
+            cxt_yield(['unevaluatedProperties'])
+          end
+        end
+
         element.add_action(:validate) do
           next if !keyword?('unevaluatedProperties')
           next if !instance.respond_to?(:to_hash)
