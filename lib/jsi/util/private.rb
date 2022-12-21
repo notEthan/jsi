89f15717d734fa346ebab70a73cc664863d2d1a7
Ethan <ethan@unth.net>
2022-12-21 16:32:26 -0600

    Util autoload MemoMap, rm MemoMap from Util, rm copied Util from MemoMap
    autoloading MemoMap eases dependency resolution since its Result class relies on AttrStruct

--- lib/jsi/util/private.rb
+++ lib/jsi/util/private.rb
@@ -6,6 +6,7 @@ module JSI
   # @api private
   module Util::Private
     autoload :AttrStruct, 'jsi/util/private/attr_struct'
+    autoload :MemoMap, 'jsi/util/private/memo_map'
 
     EMPTY_ARY = [].freeze
 
@@ -100,51 +101,6 @@ module JSI
       # overrides Kernel#hash
       def hash
         jsi_fingerprint.hash
-      end
-    end
-
-    class MemoMap
-      Result = AttrStruct[*%w(
-        value
-        inputs
-        inputs_hash
-      )]
-
-      class Result
-      end
-
-      def initialize(key_by: nil, &block)
-        @key_by = key_by
-        @block = block
-
-        # each result has its own mutex to update its memoized value thread-safely
-        @result_mutexes = {}
-        # another mutex to thread-safely initialize each result mutex
-        @result_mutexes_mutex = Mutex.new
-
-        @results = {}
-      end
-
-      def [](**inputs)
-        if @key_by
-          key = @key_by.call(**inputs)
-        else
-          key = inputs
-        end
-        result_mutex = @result_mutexes_mutex.synchronize do
-          @result_mutexes[key] ||= Mutex.new
-        end
-
-        result_mutex.synchronize do
-          inputs_hash = inputs.hash
-          if @results.key?(key) && inputs_hash == @results[key].inputs_hash && inputs == @results[key].inputs
-            @results[key].value
-          else
-            value = @block.call(**inputs)
-            @results[key] = Result.new(value: value, inputs: inputs, inputs_hash: inputs_hash)
-            value
-          end
-        end
       end
     end
 
