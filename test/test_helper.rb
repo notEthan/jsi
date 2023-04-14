d09336f35463b46d467e06e0a9f02f1565751aae
Ethan <ethan@unth.net>
2023-04-14 03:07:16 -0700

    test/jsi_helper set up bundler, debugger

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,17 +25,6 @@ if ENV['CI'] || ENV['COV']
   end
 end
 
-require 'bundler'
-bundler_groups = [:default, :test]
-bundler_groups << :dev unless ENV['CI']
-bundler_groups << :extdep if ENV['JSI_TEST_EXTDEP']
-Bundler.setup(*bundler_groups)
-
-if !ENV['CI'] && Bundler.load.specs.any? { |spec| spec.name == 'debug' }
-  require 'debug'
-  Object.alias_method(:dbg, :debugger)
-end
-
 require_relative 'jsi_helper'
 
 require 'yaml'
