561a7ed31970fec6e9331cce91e3ac1cede5ed74
Ethan <ethan@unth.net>
2023-04-17 21:43:37 -0700

    Util::MemoMap::Immutable, with #[] copied from Mutable

--- lib/jsi/util/private/memo_map.rb
+++ lib/jsi/util/private/memo_map.rb
@@ -53,5 +53,26 @@ module JSI
         end
       end
     end
+
+    class MemoMap::Immutable < MemoMap
+      def [](**inputs)
+        key = key_for(inputs)
+
+        result_mutex = @result_mutexes_mutex.synchronize do
+          @result_mutexes[key] ||= Mutex.new
+        end
+
+        result_mutex.synchronize do
+          inputs_hash = inputs.hash
+          if @results.key?(key) && inputs_hash == @results[key].inputs_hash && inputs == @results[key].inputs
+            @results[key].value
+          else
+            value = @block.call(**inputs)
+            @results[key] = Result.new(value: value, inputs: inputs, inputs_hash: inputs_hash)
+            value
+          end
+        end
+      end
+    end
   end
 end
