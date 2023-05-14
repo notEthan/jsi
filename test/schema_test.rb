da19e1e0dd4d96026692d2edec088e035673e5e5
Ethan <ethan@unth.net>
2023-05-14 00:53:58 -0700

    test regen resources for stringification all draft 7 keywords

--- test/schema_test.rb
+++ test/schema_test.rb
@@ -902,6 +902,9 @@ describe JSI::Schema do
 
     it '#pretty_print' do
       pppath = JSI::TEST_RESOURCES_PATH.join('schema.thorough.pretty_print')
+      if ENV['JSI_TEST_REGEN']
+        pppath.write(schema.pretty_inspect)
+      end
       assert_equal(pppath.read, schema.pretty_inspect)
     end
   end
