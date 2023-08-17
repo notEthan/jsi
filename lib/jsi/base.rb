e3f8ecacef0380fe85f2b3e5c7ac415abf677a28
Ethan <ethan@unth.net>
2023-08-17 15:12:48 -0700

    Base#jsi_fingerprint includes jsi_schema_registry

--- lib/jsi/base.rb
+++ lib/jsi/base.rb
@@ -660,6 +660,8 @@ module JSI
         jsi_ptr: jsi_ptr,
         # for instances in documents with schemas:
         jsi_resource_ancestor_uri: jsi_resource_ancestor_uri,
+        # different registries mean references may resolve to different resources so must not be equal
+        jsi_schema_registry: jsi_schema_registry,
       }
     end
     include Util::FingerprintHash
