6198726d9fda20169add0ec058a297968c094ad5
Ethan <ethan@unth.net>
2024-01-04 12:54:10 -0800

    Schema::Elements::DYNAMIC_REF helper proc resolve_dynamicRef init

--- lib/jsi/schema/elements/dynamic_ref.rb
+++ lib/jsi/schema/elements/dynamic_ref.rb
@@ -4,6 +4,8 @@ module JSI
   module Schema::Elements
     DYNAMIC_REF = element_map do
       Schema::Element.new(keyword: '$dynamicRef') do |element|
+        resolve_dynamicRef = proc do
+        end
       end
     end
   end
