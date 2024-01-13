951b60d7f606fad63a7e21c8a1a01751c38f861a
Ethan <ethan@unth.net>
2024-01-13 11:44:06 -0800

    JSONSchemaDraft202012 vocabulary schema module names

--- lib/schemas/json-schema.org/draft/2020-12/schema.rb
+++ lib/schemas/json-schema.org/draft/2020-12/schema.rb
@@ -27,4 +27,27 @@ module JSI
 
   module JSONSchemaDraft202012
   end
+
+  JSONSchemaDraft202012::Core       = jsi_schema_registry.find("https://json-schema.org/draft/2020-12/meta/core").jsi_schema_module
+  JSONSchemaDraft202012::Applicator  = jsi_schema_registry.find("https://json-schema.org/draft/2020-12/meta/applicator").jsi_schema_module
+  JSONSchemaDraft202012::Unevaluated  = jsi_schema_registry.find("https://json-schema.org/draft/2020-12/meta/unevaluated").jsi_schema_module
+  JSONSchemaDraft202012::Validation    = jsi_schema_registry.find("https://json-schema.org/draft/2020-12/meta/validation").jsi_schema_module
+  JSONSchemaDraft202012::MetaData       = jsi_schema_registry.find("https://json-schema.org/draft/2020-12/meta/meta-data").jsi_schema_module
+  JSONSchemaDraft202012::FormatAnnotation = jsi_schema_registry.find("https://json-schema.org/draft/2020-12/meta/format-annotation").jsi_schema_module
+  JSONSchemaDraft202012::Content         = jsi_schema_registry.find("https://json-schema.org/draft/2020-12/meta/content").jsi_schema_module
+
+  module JSONSchemaDraft202012::Core
+  end
+  module JSONSchemaDraft202012::Applicator
+  end
+  module JSONSchemaDraft202012::Unevaluated
+  end
+  module JSONSchemaDraft202012::Validation
+  end
+  module JSONSchemaDraft202012::MetaData
+  end
+  module JSONSchemaDraft202012::FormatAnnotation
+  end
+  module JSONSchemaDraft202012::Content
+  end
 end
