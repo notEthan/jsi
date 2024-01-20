119da486488ff389639af78100c66fa709524e4c
Ethan <ethan@unth.net>
2024-01-20 12:09:01 -0800

    unevaluatedItems, unevaluatedProperties depend on other in-place and child applicator elements

--- lib/jsi/schema/elements/unevaluated_properties.rb
+++ lib/jsi/schema/elements/unevaluated_properties.rb
@@ -5,6 +5,11 @@ module JSI
     # https://json-schema.org/draft/2020-12/draft-bhutton-json-schema-01#name-unevaluatedproperties
     UNEVALUATED_PROPERTIES = element_map do
       Schema::Element.new(keyword: 'unevaluatedProperties') do |element|
+        element.depends_on_elements do |other_element|
+          other_element.invokes?(:inplace_applicate) ||
+            (other_element.invokes?(:child_applicate) && !other_element.invokes?(:application_requires_evaluated))
+        end
+
         element.add_action(:application_requires_evaluated) { cxt_yield(true) if keyword?('unevaluatedProperties') }
 
         element.add_action(:subschema) do
