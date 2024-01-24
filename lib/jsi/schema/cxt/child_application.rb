36a68ad977746b61eee54a3c1a330926ae34fad0
Ethan <ethan@unth.net>
2024-01-24 01:14:41 -0800

    Schema::Cxt::ChildApplication#evaluated

--- lib/jsi/schema/cxt/child_application.rb
+++ lib/jsi/schema/cxt/child_application.rb
@@ -5,8 +5,12 @@ module JSI
     ChildApplication = Block.subclass(*%w(
       instance
       token
+      evaluated
     ))
 
+    # @!attribute evaluated
+    #   Was the child successfully evaluated by a child applicator?
+    #   @return [Boolean]
     class ChildApplication
       # @param subschema_ptr [Ptr, #to_ary]
       def child_subschema_applicate(subschema_ptr)
