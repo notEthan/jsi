b2c3ae5ee8946c0e70db04c0923173f7cc3d4039
Ethan <ethan@unth.net>
2024-01-24 20:09:55 -0800

    Schema::Elements::CONTENT_SCHEMA action :subschema

--- lib/jsi/schema/elements/content_schema.rb
+++ lib/jsi/schema/elements/content_schema.rb
@@ -4,6 +4,12 @@ module JSI
   module Schema::Elements
     CONTENT_SCHEMA = element_map do
       Schema::Element.new(keyword: 'contentSchema') do |element|
+        element.add_action(:subschema) do
+          if keyword?('contentSchema')
+            #> The value of this property MUST be a valid JSON schema.
+            cxt_yield(['contentSchema'])
+          end
+        end # element.add_action(:subschema)
       end # Schema::Element.new
     end # CONTENT_SCHEMA = element_map
   end # module Schema::Elements
