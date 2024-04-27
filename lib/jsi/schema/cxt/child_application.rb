90d6268005a3d6e7f93e003a8a4cd4dd8e9e43e3
Ethan <ethan@unth.net>
2024-04-26 19:36:32 -0700

    Schema::Cxt::ChildApplication#child_schema_applicate

--- lib/jsi/schema/cxt/child_application.rb
+++ lib/jsi/schema/cxt/child_application.rb
@@ -10,7 +10,12 @@ module JSI
     class ChildApplication
       # @param subschema_ptr [Ptr, #to_ary]
       def child_subschema_applicate(subschema_ptr)
-        cxt_yield(schema.subschema(subschema_ptr))
+        child_schema_applicate(schema.subschema(subschema_ptr))
+      end
+
+      # @param child_applicator_schema [Schema]
+      def child_schema_applicate(child_applicator_schema)
+        cxt_yield(child_applicator_schema)
       end
     end
   end
