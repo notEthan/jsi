38c08e1ad3fd5e24812a8f9bf42abb00f2db41dd
Ethan <ethan@unth.net>
2024-06-21 18:52:59 -0700

    fix references /JSONSchemaOrgDraft0X/JSONSchemaDraft0X/

--- test/new_jsi_test.rb
+++ test/new_jsi_test.rb
@@ -3,10 +3,10 @@ require_relative 'test_helper'
 describe 'new_jsi, new_schema' do
   describe 'new_schema' do
     it 'initializes with a block' do
-      schema1 = JSI.new_schema({'$id' => 'tag:gxif'}, default_metaschema: JSI::JSONSchemaOrgDraft07) do
+      schema1 = JSI.new_schema({'$id' => 'tag:gxif'}, default_metaschema: JSI::JSONSchemaDraft07) do
         define_method(:foo) { :foo }
       end
-      schema2 = JSI::JSONSchemaOrgDraft07.new_schema({'$id' => 'tag:ijpa'}) do
+      schema2 = JSI::JSONSchemaDraft07.new_schema({'$id' => 'tag:ijpa'}) do
         define_method(:foo) { :foo }
       end
       assert_equal(:foo, schema1.new_jsi([]).foo)
