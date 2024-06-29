97b3f35228a79219b136306bacff5792ecbebafe
Ethan <ethan@unth.net>
2024-06-28 17:10:14 -0700

    Schema::Elements::REF rename /throw_done/exclusive/

--- lib/jsi/schema/elements/ref.rb
+++ lib/jsi/schema/elements/ref.rb
@@ -3,14 +3,14 @@
 module JSI
   module Schema::Elements
     REF = element_map do
-      throw_done = true
+      exclusive = true
       Schema::Element.new do |element|
         element.add_action(:inplace_applicate) do
       if keyword?('$ref') && schema_content['$ref'].respond_to?(:to_str)
         ref = schema.schema_ref('$ref')
         unless visited_refs.include?(ref)
           ref.deref_schema.each_inplace_applicator_schema(instance, visited_refs: visited_refs + [ref], &block)
-          if throw_done
+          if exclusive
             throw(:jsi_application_done)
           end
         end
