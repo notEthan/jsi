dbf56c824bd00631f545037f076324eaa28ace3c
Ethan <ethan@unth.net>
2024-10-10 11:41:56 -0700

    URI#initialize freeze

--- lib/jsi/uri.rb
+++ lib/jsi/uri.rb
@@ -3,6 +3,7 @@
 module JSI
   # JSI::URI adds to Addressable::URI:
   #
+  # - always immutable
   # - `JSI::URI["http://x"]` parses, and JSI::URI#inspect shows this form, copy/pastable
   class URI < Addressable::URI
     class << self
@@ -11,6 +12,16 @@ module JSI
       def [](uri)
         parse(uri)
       end
+    end
+
+    def initialize(options={})
+      super
+      freeze
+    end
+
+    def merge(hash)
+      # Addressable::URI#merge instantiates and mutates, not compatible with #initialize freezing. work around.
+      self.class.new(Addressable::URI.new(to_hash).merge(hash).to_hash)
     end
 
     # @return [String]
