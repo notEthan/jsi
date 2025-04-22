d722f9be719eb4b6d557c94d4e80093921b5fef4
Ethan <ethan@unth.net>
2025-04-22 00:21:19 -0700

    rm deprecated Validation::Result#validation_errors

--- lib/jsi/validation/result.rb
+++ lib/jsi/validation/result.rb
@@ -137,12 +137,6 @@ module JSI
         nil
       end
 
-      # @deprecated after v0.8
-      # iterating (recursively) is better done with #each_validation_error
-      def validation_errors
-        each_validation_error.to_set
-      end
-
       # @return [Set]
       attr_reader(:evaluated_tokens)
 
