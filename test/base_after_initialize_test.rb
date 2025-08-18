467b935770eacb4a6ab13d220a3f7237fdb2eb22
Ethan <ethan@unth.net>
2025-08-18 01:49:28 -0700

    test after_initialize modified copy

--- test/base_after_initialize_test.rb
+++ test/base_after_initialize_test.rb
@@ -36,6 +36,38 @@ describe("Base after_initialize") do
       assert_equal(Set[JSI::Ptr[], JSI::Ptr['foo']], ptrs)
     end
   end
+
+  describe("modified copy") do
+    it("is not called on a modified copy") do
+      nodes = Set[]
+      metaschema = JSI.new_metaschema_node(BasicMetaSchema.schema_content,
+        dialect: BASIC_DIALECT,
+        after_initialize: nodes.method(:<<),
+      )
+      assert_equal(metaschema.jsi_each_descendent_node.to_set, nodes)
+      metaschema.merge({'modified' => true}).jsi_each_descendent_node { }
+      # unchanged
+      assert_equal(metaschema.jsi_each_descendent_node.to_set, nodes)
+
+      nodes = Set[]
+      schema = metaschema.new_schema({'additionalProperties' => {}},
+        after_initialize: nodes.method(:<<),
+      )
+      assert_equal(schema.jsi_each_descendent_node.to_set, nodes)
+      schema.merge({'modified' => true}).jsi_each_descendent_node { }
+      # unchanged
+      assert_equal(schema.jsi_each_descendent_node.to_set, nodes)
+
+      nodes = Set[]
+      jsi = schema.new_jsi({'foo' => {}},
+        after_initialize: nodes.method(:<<),
+      )
+      assert_equal(jsi.jsi_each_descendent_node.to_set, nodes)
+      jsi.merge({'modified' => true}).jsi_each_descendent_node { }
+      # unchanged
+      assert_equal(jsi.jsi_each_descendent_node.to_set, nodes)
+    end
+  end
 end
 
 $test_report_file_loaded[__FILE__]
