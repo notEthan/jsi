1e5b3a2e9225602758e04e1233733253f88e0ca3
Ethan <ethan@unth.net>
2025-08-18 10:29:22 -0700

    doc SchemaSet#new_jsi reword #jsi_schemas #jsi_indicated_schemas

--- lib/jsi/schema_set.rb
+++ lib/jsi/schema_set.rb
@@ -68,8 +68,10 @@ module JSI
     end
 
     # Instantiates a new JSI whose content comes from the given `instance` param.
-    # This SchemaSet indicates the schemas of the JSI - its schemas are in-place
+    #
+    # The schemas of the JSI (its {Base#jsi_schemas}) are in-place
     # applicators of this set's schemas which apply to the given instance.
+    # The JSI's {Base#jsi_indicated_schemas} set is this set.
     #
     # @param instance [Object] the instance to be represented as a JSI
     # @param uri [#to_str, URI] The retrieval URI of the instance.
