3bb61b7107f069a0035c45756db8407203c3229b
Ethan <ethan@unth.net>
2025-12-02 11:56:54 -0800

    JSONSchemaDraft202012#jsi_schema_module_connection_created defines #defs reader for 202012 schema modules

--- lib/jsi/schema_classes.rb
+++ lib/jsi/schema_classes.rb
@@ -152,11 +152,6 @@ module JSI
     def with_dynamic_scope_from(node)
       schema.with_dynamic_scope_from(node).jsi_schema_module
     end
-
-    # `$defs` property reader
-    def defs
-      self['$defs']
-    end
   end
 
   # A module to extend the {SchemaModule} of a schema which describes other schemas (a {Schema::MetaSchema})
