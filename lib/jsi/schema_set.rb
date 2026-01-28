78ff6696be622a2f9cd7ab23a2657aa079ded0e3
Ethan <ethan@unth.net>
2026-01-28 13:11:04 -0800

    SchemaSet rm compare_by_identity hax

--- lib/jsi/schema_set.rb
+++ lib/jsi/schema_set.rb
@@ -5,9 +5,6 @@ module JSI
   #
   # any schema instance is described by a set of schemas.
   class SchemaSet < Set
-    COMPARE_BY_IDENTITY_DEFINED = method_defined?(:compare_by_identity)
-    private_constant(:COMPARE_BY_IDENTITY_DEFINED)
-
     class << self
       # Builds a SchemaSet, yielding a yielder to be called with each schema of the SchemaSet.
       #
@@ -42,13 +39,8 @@ module JSI
       end
 
       super(&nil) # note super() does implicitly pass block without &nil
-      if COMPARE_BY_IDENTITY_DEFINED
-        compare_by_identity
-      else
-        # TODO rm when Set#compare_by_identity is universally available.
-        # note does not work on JRuby, but JRuby has Set#compare_by_identity.
-        @hash.compare_by_identity
-      end
+
+      compare_by_identity
 
       if block
         enum.each_entry { |o| add(block[o]) }
